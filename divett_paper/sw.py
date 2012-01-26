e9c4cf87217b9761da8941e20c6d5f6f1da6ff83
Simon Funke <simon.funke@gmail.com>
2012-01-26 00:23:07 +0000

    The divett setup works with the new setup

--- divett_paper/sw.py
+++ divett_paper/sw.py
@@ -1,51 +1,51 @@
 import sys
-import divett
+import sw_config 
 import sw_lib
 from dolfin import *
 from dolfin_adjoint import *
 
-W=sw_lib.p1dgp2(divett.mesh)
-
-state=Function(W)
-
-
+config = sw_config.SWConfiguration(nx=30, ny=2) 
 period = 1.24*60*60 # Wave period
-divett.params["k"]=2*pi/(period*sqrt(divett.params["g"]*divett.params["depth"]))
-divett.params["basename"]="p1dgp2"
-divett.params["finish_time"]=period
-divett.params["dt"]=divett.params["finish_time"]/100
+config.params["k"]=2*pi/(period*sqrt(config.params["g"]*config.params["depth"]))
+config.params["basename"]="p1dgp2"
+config.params["finish_time"]=period
+config.params["dt"]=config.params["finish_time"]/100
 print "Wave period (in h): ", period/60/60 
-divett.params["dump_period"]=1
+config.params["dump_period"]=1
+config.params["bctype"]="flather"
 
 class InitialConditions(Expression):
     def __init__(self):
         pass
     def eval(self, values, X):
-        values[0]=divett.params['eta0']*sqrt(divett.params['g']*divett.params['depth'])*cos(divett.params["k"]*X[0])
+        values[0]=config.params['eta0']*sqrt(config.params['g']*config.params['depth'])*cos(config.params["k"]*X[0])
         values[1]=0.
-        values[2]=divett.params['eta0']*cos(divett.params["k"]*X[0])
+        values[2]=config.params['eta0']*cos(config.params["k"]*X[0])
     def value_shape(self):
         return (3,)
 
+W=sw_lib.p1dgp2(config.mesh)
+
+state=Function(W)
 state.interpolate(InitialConditions())
 
-M,G,rhs_contr,ufl,ufr=sw_lib.construct_shallow_water(W,divett.ds,divett.params)
+M,G,rhs_contr,ufl,ufr=sw_lib.construct_shallow_water(W, config.ds, config.params)
 
-state = sw_lib.timeloop_theta(M,G,rhs_contr,ufl,ufr,state,divett.params)
+state = sw_lib.timeloop_theta(M, G, rhs_contr, ufl, ufr, state, config.params)
 sys.exit()
 
 adj_html("sw_forward.html", "forward")
 adj_html("sw_adjoint.html", "adjoint")
-sw_lib.replay(state, divett.params)
+sw_lib.replay(state, config.params)
 
 J = Functional(dot(state, state)*dx)
 f_direct = assemble(dot(state, state)*dx)
-adj_state = sw_lib.adjoint(state, divett.params, J)
+adj_state = sw_lib.adjoint(state, config.params, J)
 
 ic = Function(W)
-ic.interpolate(InitialConditions())
+ic.interpolate(config.InitialConditions())
 def J(ic):
-  state = sw_lib.timeloop_theta(M, G, rhs_contr,ufl,ufr,ic, divett.params, annotate=False)
+  state = sw_lib.timeloop_theta(M, G, rhs_contr,ufl,ufr,ic, config.params, annotate=False)
   return assemble(dot(state, state)*dx)
 
 minconv = test_initial_condition(J, ic, adj_state, seed=0.001)
