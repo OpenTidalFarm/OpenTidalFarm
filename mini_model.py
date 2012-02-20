6d579bc4859bd68853209c11ea48fef3bf9b7f9f
Simon Funke <simon.funke@gmail.com>
2012-02-20 22:54:09 +0000

    A new test that finds the optimal friction using an analytical flow model

---
+++ mini_model.py
@@ -0,0 +1,55 @@
+import sw_lib
+import numpy
+from dolfin import *
+from dolfin_adjoint import *
+
+def construct_mini_model(W, params, turbine_field):
+    (v, q)=TestFunctions(W)
+    (u, h)=TrialFunctions(W)
+
+    # Mass matrix
+    M=inner(v,u)*dx
+    M+=inner(q,h)*dx
+
+    A = (1.0+turbine_field)*inner(v,u)*dx
+    A += inner(q,h)*dx
+
+    rhs = action(M, Constant((2.0, 0.0, 0.0)))
+    return A, rhs
+
+
+def mini_model(A, rhs, state, params, time_functional=None, annotate=True):
+    '''Solve (1+turbine)*M*state = (2, 0, 0)*M. 
+       The solutioin is a x-velocity of 2/(turbine+1) and a pressure and y-velocity of 0. 
+    '''
+    
+    u_out, p_out = sw_lib.output_files(params["basename"])
+    M_u_out, v_out, u_out_state=sw_lib.u_output_projector(state.function_space())
+    M_p_out, q_out, p_out_state=sw_lib.p_output_projector(state.function_space())
+
+    tmpstate=Function(state.function_space())
+
+    # Solve the mini model 
+    solver_parameters = {"linear_solver": "cg", "preconditioner": "sor"}
+    solve(A==rhs, tmpstate, solver_parameters=solver_parameters, annotate=annotate)
+
+    state.assign(tmpstate, annotate=annotate)
+
+    # Project the solution to P1 for visualisation.
+    rhs_p = assemble(inner(v_out,state.split()[0])*dx)
+    solve(M_u_out, u_out_state.vector(), rhs_p, "cg", "sor", annotate=False) 
+
+    # Project the solution to P1 for visualisation.
+    rhs_p = assemble(inner(q_out,state.split()[1])*dx)
+    solve(M_p_out, p_out_state.vector(), rhs_p, "cg", "sor", annotate=False) 
+    
+    u_out << u_out_state
+    p_out << p_out_state
+
+    if time_functional is not None:
+      j = assemble(time_functional.Jt(state)) 
+      djdm = numpy.array([assemble(f) for f in time_functional.dJtdm(state)])
+      return j, djdm, state 
+    else: 
+      return state
+
---
+++ test_optimal_friction/sw.py
@@ -0,0 +1,168 @@
+import sys
+import cProfile
+import pstats
+import sw_config 
+import sw_lib
+import numpy
+import Memoize
+import IPOptUtils
+from functionals import DefaultFunctional
+from sw_utils import test_initial_condition_adjoint, test_gradient_array
+from turbines import *
+from mini_model import *
+from dolfin import *
+from dolfin_adjoint import *
+
+# Global counter variable for vtk output
+count = 0
+
+def default_config():
+  # We set the perturbation_direction with a constant seed, so that it is consistent in a parallel environment.
+  numpy.random.seed(21) 
+  config = sw_config.DefaultConfiguration(nx=20, ny=10)
+  config.params["verbose"] = 0
+
+  # dt is used in the functional only, so we set it here to 1.0
+  config.params["dt"] = 1.0
+  # Turbine settings
+  config.params["turbine_pos"] = [[500., 500.]]
+  # The turbine friction is the control variable 
+  config.params["turbine_friction"] = 12.0*numpy.random.rand(len(config.params["turbine_pos"]))
+  config.params["turbine_length"] = 8000
+  config.params["turbine_width"] = 8000
+  config.params["turbine_model"] = "ConstantTurbine"
+
+  return config
+
+def initial_control(config):
+  # We use the current turbine settings as the intial control
+  res = config.params['turbine_friction'].tolist()
+  #res += numpy.reshape(config.params['turbine_pos'], -1).tolist()
+  return numpy.array(res)
+
+def j_and_dj(m):
+  adjointer.reset()
+  adj_variables.__init__()
+
+  # Change the control variables to the config parameters
+  config.params["turbine_friction"] = m[:len(config.params["turbine_friction"])]
+  mp = m[len(config.params["turbine_friction"]):]
+  #config.params["turbine_pos"] = numpy.reshape(mp, (-1, 2))
+
+  set_log_level(30)
+  debugging["record_all"] = True
+
+  W=sw_lib.p1dgp2(config.mesh)
+  state=Function(W)
+
+  # Set the control values
+  U = W.split()[0].sub(0) # Extract the first component of the velocity function space 
+  U = U.collapse() # Recompute the DOF map
+  tf = Function(U) # The turbine function
+  tfd = Function(U) # The derivative turbine function
+
+  # Set up the turbine friction field using the provided control variable
+  tf.interpolate(Turbines(config.params))
+
+  global count
+  count+=1
+  sw_lib.save_to_file_scalar(tf, "turbines_"+str(count))
+
+  A, rhs = construct_mini_model(W, config.params, tf)
+
+  functional = DefaultFunctional(config.params)
+
+  # Solve the shallow water system
+  j, djdm, state = mini_model(A, rhs, state, config.params, functional)
+  #print "Layout power extraction: ", j/1000000, " MW."
+  #print "Which is equivalent to a average power generation of: ",  j/1000000/(config.params["current_time"]-config.params["start_time"]), " MW"
+
+  sw_lib.replay(state, config.params)
+
+  J = TimeFunctional(functional.Jt(state))
+  adj_state = sw_lib.adjoint(state, config.params, J, until=0) # The first annotation is the idendity operator for the turbine field
+
+  # Let J be the functional, m the parameter and u the solution of the PDE equation F(u) = 0.
+  # Then we have 
+  # dJ/dm = (\partial J)/(\partial u) * (d u) / d m + \partial J / \partial m
+  #               = adj_state * \partial F / \partial u + \partial J / \partial m
+  # In this particular case m = turbine_friction, J = \sum_t(ft) 
+  dj = [] 
+  v = adj_state.vector()
+  # Compute the derivatives with respect to the turbine friction
+  for n in range(len(config.params["turbine_friction"])):
+    tfd.interpolate(Turbines(config.params, derivative_index_selector=n, derivative_var_selector='turbine_friction'))
+    dj.append( v.inner(tfd.vector()) )
+
+  # Compute the derivatives with respect to the turbine position
+  for n in range(len(config.params["turbine_pos"])):
+    for var in ('turbine_pos_x', 'turbine_pos_y'):
+      tfd.interpolate(Turbines(config.params, derivative_index_selector=n, derivative_var_selector=var))
+      dj.append( v.inner(tfd.vector()) )
+  dj = numpy.array(dj)  
+  
+  # Now add the \partial J / \partial m term
+  dj += djdm
+
+  return j, dj 
+
+j_and_dj_mem = Memoize.MemoizeMutable(j_and_dj)
+def j(m):
+  j = j_and_dj_mem(m)[0]
+  print 'Evaluating j(', m.__repr__(), ')=', j
+  return j 
+
+def dj(m):
+  dj = j_and_dj_mem(m)[1]
+  print 'Evaluating dj(', m.__repr__(), ')=', dj
+  # Return only the derivatives with respect to the friction
+  return dj[:len(config.params['turbine_friction'])]
+
+config = default_config()
+m0 = initial_control(config)
+
+# We set the perturbation_direction with a constant seed, so that it is consistent in a parallel environment.
+p = numpy.random.rand(len(config.params['turbine_friction']))
+minconv = test_gradient_array(j, dj, m0, seed=0.001, perturbation_direction=p)
+if minconv < 1.99:
+  print "The gradient taylor remainder test failed."
+  sys.exit(1)
+
+opt_package = 'ipopt'
+
+if opt_package == 'ipopt':
+  # If this option does not produce any ipopt outputs, delete the ipopt.opt file
+  import ipopt 
+  g = lambda m: []
+  dg = lambda m: []
+
+  f = IPOptUtils.IPOptFunction()
+  # Overwrite the functional and gradient function with our implementation
+  f.objective= j 
+  f.gradient= dj 
+
+  nlp = ipopt.problem(len(m0), 
+                      0, 
+                      f, 
+                      numpy.zeros(len(m0)), 
+                      100*numpy.ones(len(m0)))
+  nlp.addOption('mu_strategy', 'adaptive')
+  nlp.addOption('tol', 1e-9)
+  nlp.addOption('print_level', 5)
+  nlp.addOption('check_derivatives_for_naninf', 'yes')
+  # A -1.0 scaling factor transforms the min problem to a max problem.
+  nlp.addOption('obj_scaling_factor', -1.0)
+  # Use an approximate Hessian since we do not have second order information.
+  nlp.addOption('hessian_approximation', 'limited-memory')
+
+  m, info = nlp.solve(m0)
+  print info['status_msg']
+  print "Solution of the primal variables: m=%s\n" % repr(m) 
+  print "Solution of the dual variables: lambda=%s\n" % repr(info['mult_g'])
+  print "Objective=%s\n" % repr(info['obj_val'])
+
+  if info['status'] != 0 or abs(m[0]-0.5) > 1.0**-10: 
+    print "The optimisation algorithm did not find the correct solution."
+    sys.exit(1) 
+  else:
+    exit_code = 0
