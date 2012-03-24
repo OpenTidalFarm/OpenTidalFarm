5511904cd34b74d5187f931f9e3500e139a8bb2c
Simon Funke <simon.funke@gmail.com>
2012-03-24 13:14:24 +0000

    Add parallel support for the AnomatedPlot class

--- animated_plot.py
+++ animated_plot.py
@@ -1,8 +1,19 @@
 import numpy 
 import pylab
+from dolfin import MPI
+
+def cpu0only(f):
+  ''' A decorator class that only evaluates on the first CPU in a parallel environment. '''
+  def decorator(self, *args, **kw):
+    myid = MPI.process_number()
+    if myid == 0:
+      f(self, *args, **kw)
+
+  return decorator
 
 class AnimatedPlot:
   ''' A class that implements animated plots with equidistant x values. New points are added by calling the addPoint function. '''
+  @cpu0only 
   def __init__(self, xlabel=None, ylabel=None):
     self.datay = []
     pylab.ion() # animation on
@@ -13,8 +24,13 @@ class AnimatedPlot:
     if ylabel:
       pylab.ylabel(ylabel)
   
+  @cpu0only 
   def addPoint(self, value):
     ''' Adds a new point and updates the plot. '''
+    #myid = MPI.process_number()
+    #if myid != 0:
+    #  return
+
     datay = self.datay
     line = self.line
 
@@ -24,6 +40,7 @@ class AnimatedPlot:
     line.set_ydata(datay)
     pylab.draw() # draw the points again
 
+  @cpu0only 
   def savefig(self, name):
     pylab.savefig(name)
 
