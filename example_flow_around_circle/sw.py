7d0982e200313fd4c56ee7e3f8f5f4713c177bc0
Simon Funke <simon.funke@gmail.com>
2012-04-02 10:57:39 +0100

    An example that solves the flow around a circle to study the turbulence behaviuour of the model

---
+++ example_flow_around_circle/sw.py
@@ -0,0 +1,103 @@
+''' An example that solves the flow around a circle. The purpose of this example is to investigate the turbulence behind a turbine. ''' 
+
+import sys
+import cProfile
+import pstats
+import sw_config 
+import sw_lib
+import numpy
+import Memoize
+from animated_plot import *
+from functionals import DefaultFunctional, build_turbine_cache
+from sw_utils import test_initial_condition_adjoint, test_gradient_array, pprint
+from turbines import *
+from mini_model import *
+from dolfin import *
+from dolfin_adjoint import *
+
+def default_config():
+  # We set the perturbation_direction with a constant seed, so that it is consistent in a parallel environment.
+  config = sw_config.DefaultConfiguration(mesh_file = 'mesh.xml')
+  period = 1.24*60*60 # Wave period
+  config.params["k"] = 2*pi/(period*sqrt(config.params["g"]*config.params["depth"]))
+  pprint("Wave period (in h): ", period/60/60)
+  config.params["dump_period"] = 1
+  config.params["verbose"] = 0
+
+  # Start at rest state
+  config.params["start_time"] = period/4
+  config.params["dt"] = period/80
+  config.params["finish_time"] = 4.*period/4 
+  config.params["theta"] = 0.6
+  config.params["include_advection"] = True 
+  config.params["include_diffusion"] = True 
+  config.params["diffusion_coef"] = 2.0
+  config.params["newton_solver"] = False 
+  config.params["picard_iterations"] = 20
+  config.params['basename'] = "p2p1"
+  config.params["run_benchmark"] = False 
+  config.params['solver_exclude'] = ['cg', 'lu']
+  info_green("Approximate CFL number (assuming a velocity of 2): " +str(2*config.params["dt"]/config.mesh.hmin())) 
+
+  #set_log_level(DEBUG)
+  set_log_level(20)
+  #dolfin.parameters['optimize'] = True
+  #dolfin.parameters['optimize_use_dofmap_cache'] = True
+  #dolfin.parameters['optimize_use_tensor_cache'] = True
+  #dolfin.parameters['form_compiler']['optimize'] = True
+  dolfin.parameters['form_compiler']['cpp_optimize'] = True
+  dolfin.parameters['form_compiler']['cpp_optimize_flags'] = '-O3'
+
+  # Turbine settings
+  config.params["quadratic_friction"] = True
+  config.params["friction"] = 0.0025
+  return config
+
+def initial_control(config):
+  # We use the current turbine settings as the intial control
+  res = numpy.reshape(config.params['turbine_pos'], -1).tolist()
+  return numpy.array(res)
+
+def j(m):
+  adj_reset()
+
+  # Change the control variables to the config parameters
+  config.params["turbine_pos"] = numpy.reshape(m, (-1, 2))
+
+  set_log_level(20)
+  debugging["record_all"] = True
+
+  W = sw_lib.p2p1(config.mesh)
+  state=Function(W)
+  state.interpolate(config.get_sin_initial_condition()())
+
+  # Set the control values
+  U = W.split()[0].sub(0) # Extract the first component of the velocity function space 
+  U = U.collapse() # Recompute the DOF map
+  tf = Function(U) # The turbine function
+
+  # Set up the turbine friction field using the provided control variable
+  turbines = Turbines(config.params)
+  tf.interpolate(turbines)
+
+  sw_lib.save_to_file_scalar(tf, "turbines_t=.0.x")
+
+  # Scale the turbines in the functional for a physically consistent power/friction curve
+  turbine_cache = build_turbine_cache(config.params, U, turbine_size_scaling=0.5)
+  functional = DefaultFunctional(config.params, turbine_cache)
+
+  # Solve the shallow water system
+  j, djdm = sw_lib.sw_solve(W, config, state, turbine_field = tf, time_functional=functional, linear_solver='lu', preconditioner='none')
+
+  return j
+
+config = default_config()
+m0 = initial_control(config)
+j0 = j(m0)
+pprint("Power outcome: ", j0)
+pprint("Timing summary:")
+timer = Timer("NULL")
+timer.stop()
+
+list_timings()
+
