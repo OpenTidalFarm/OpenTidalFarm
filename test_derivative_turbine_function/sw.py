f35edbee3bd28fc1321b7673835078d205b4aee8
Simon Funke <simon.funke@gmail.com>
2012-04-04 15:28:53 +0100

    I was too enthuastic in changing test_derivative_turbine_function - revert it back to the original test

--- test_derivative_turbine_function/sw.py
+++ test_derivative_turbine_function/sw.py
@@ -6,9 +6,13 @@
 import sys
 import configuration 
 import numpy
+import function_spaces
 from dolfin import *
 from helpers import test_gradient_array
 from reduced_functional import ReducedFunctional 
+from turbines import *
+from initial_conditions import SinusoidalInitialCondition
+set_log_level(PROGRESS)
 
 def default_config():
   numpy.random.seed(21) 
@@ -25,19 +29,61 @@ def default_config():
 
   return config
 
+def j_and_dj(m):
+  # Change the control variables to the config parameters
+  config.params["turbine_friction"] = m[:len(config.params["turbine_friction"])]
+  mp = m[len(config.params["turbine_friction"]):]
+  config.params["turbine_pos"] = numpy.reshape(mp, (-1, 2))
+
+
+  W = function_spaces.p1dgp2(config.mesh)
+
+  # Get initial conditions
+  state=Function(W, name = "current_state")
+  state.interpolate(SinusoidalInitialCondition(config)())
+
+  # Set the control values
+  U = W.split()[0].sub(0) # Extract the first component of the velocity function space 
+  U = U.collapse() # Recompute the DOF map
+  tf = Function(U, name = "turbine") # The turbine function
+  tfd = Function(U, name = "turbine_derivative") # The derivative turbine function
+
+  # Set up the turbine friction field using the provided control variable
+  tf.interpolate(Turbines(config.params))
+  v = tf.vector()
+  # The functional of interest is simply the l2 norm of the turbine field
+  j = v.inner(v)  
+
+  dj = []
+  # Compute the derivatives with respect to the turbine friction
+  for n in range(len(config.params["turbine_friction"])):
+    tfd.interpolate(Turbines(config.params, derivative_index_selector=n, derivative_var_selector='turbine_friction'))
+    dj.append( 2 * v.inner(tfd.vector()) )
+
+  # Compute the derivatives with respect to the turbine position
+  for n in range(len(config.params["turbine_pos"])):
+    for var in ('turbine_pos_x', 'turbine_pos_y'):
+      tfd.interpolate(Turbines(config.params, derivative_index_selector=n, derivative_var_selector=var))
+      dj.append( 2 * v.inner(tfd.vector()) )
+  dj = numpy.array(dj)  
+  
+  return j, dj 
+
+j = lambda m: j_and_dj(m)[0]
+dj = lambda m: j_and_dj(m)[1]
+
 # run the taylor remainder test 
 config = default_config()
-model = ReducedFunctional(config)
-m0 = model.initial_control()
+m0 = ReducedFunctional(config).initial_control()
 
 # We set the perturbation_direction with a constant seed, so that it is consistent in a parallel environment.
 p = numpy.random.rand(len(m0))
 
 # Run with a functional that does not depend on m directly
 for turbine_model, s in {'GaussianTurbine': {'seed': 100.0, 'tol': 1.9}, 'BumpTurbine': {'seed': 0.001, 'tol': 1.99}}.items():
-  print "************* ", turbine_model, " ********************"
+  info("************* %s ********************" % turbine_model)
   config.params["turbine_model"] = turbine_model 
-  minconv = test_gradient_array(model.j, model.dj, m0, s['seed'], perturbation_direction=p)
+  minconv = test_gradient_array(j, dj, m0, s['seed'], perturbation_direction=p)
 
   if minconv < s['tol']:
     sys.exit(1)
