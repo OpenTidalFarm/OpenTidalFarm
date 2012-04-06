ba6a4f618a64989ba26559a0cbfc2290f0f8e63d
Simon Funke <simon.funke@gmail.com>
2012-04-06 10:16:14 +0100

    Some minor changes to memory_report.sh

--- example_divett_setup_optimisation/memory_report.sh
+++ example_divett_setup_optimisation/memory_report.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-rm memory_report.txt
+rm -f memory_report.txt
 touch memory_report.txt
 for (( ; ; ))
 do
+   date >> memory_report.txt 
    ps -eo pid,pri,pcpu,size,cmd --sort pcpu | tail >> memory_report.txt
    sleep 1m
 done
