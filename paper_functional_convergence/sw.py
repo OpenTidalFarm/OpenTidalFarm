9b44148791f3aa3716a86b0db8fa1f6329bbfc00
Simon Funke <simon.funke@gmail.com>
2012-04-08 18:25:39 +0100

    Some initial work for paper_functional_convergence

--- paper_functional_convergence/sw.py
+++ paper_functional_convergence/sw.py
@@ -1,5 +1,4 @@
 ''' This tests checks that the power output of a single turbine in a periodic domain is independent of its position ''' 
-
 import sys
 import configuration 
 import numpy
@@ -7,29 +6,38 @@ from dirichlet_bc import DirichletBCSet
 import IPOptUtils
 import ipopt
 from helpers import test_gradient_array
-from animated_plot import *
 from reduced_functional import ReducedFunctional
 from dolfin import *
-#set_log_level(PROGRESS)
+set_log_level(ERROR)
 
-# An animated plot to visualise the development of the functional value
-plot = AnimatedPlot(xlabel='Iteration', ylabel='Functional value')
+if len(sys.argv) not in [2, 3]:
+    print "Missing command line argument: y position of the turbine"
+    print "Usage: sw.py [--fine] y_position"
+    sys.exit(1)
+
+turbine_y_pos = float(sys.argv[-1])
+
+nx = 100
+ny = 33
+if len(sys.argv) == 3 and sys.argv[1] == '--fine':
+    nx = 2*nx
+    ny = 2*ny
 
 def default_config():
   # We set the perturbation_direction with a constant seed, so that it is consistent in a parallel environment.
   numpy.random.seed(21) 
-  config = configuration.DefaultConfiguration(nx=100, ny=33)
+  config = configuration.DefaultConfiguration(nx = nx, ny = ny)
+  print "The vertical mesh element size is %f." % (config.params["basin_y"]/ny) 
   period = 1.24*60*60 # Wave period
   config.params["k"] = 2*pi/(period*sqrt(config.params["g"]*config.params["depth"]))
   info("Wave period (in h): %f" % (period/60/60) )
-  config.params["dump_period"] = 1
+  config.params["dump_period"] = 10000
   config.params["verbose"] = 0
 
   # Start at rest state
   config.params["start_time"] = period/4
   config.params["dt"] = period/50
   config.params["finish_time"] = 3.*period/4 
-  config.params["finish_time"] = config.params["start_time"] + config.params["dt"]
   config.params["theta"] = 0.6
   config.params["include_advection"] = True 
   config.params["include_diffusion"] = True 
@@ -55,7 +63,7 @@ def default_config():
   config.params["quadratic_friction"] = True
   config.params["friction"] = 0.0025
   # The turbine position is the control variable 
-  config.params["turbine_pos"] = [[1500, 500]] 
+  config.params["turbine_pos"] = [[1500., turbine_y_pos]] 
 
   info_blue("Deployed " + str(len(config.params["turbine_pos"])) + " turbines.")
   # Choosing a friction coefficient of > 0.02 ensures that overlapping turbines will lead to
@@ -71,6 +79,3 @@ model = ReducedFunctional(config, scaling_factor = 10**-4, plot = True)
 m0 = model.initial_control()
 print "Functional value: ", model.j(m0)
 print "Derivative value: ", model.dj(m0)
-from IPython.Shell import IPShellEmbed
-ipshell = IPShellEmbed()
-ipshell()
