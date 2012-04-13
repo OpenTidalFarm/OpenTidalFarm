cd9184ea1c78c66a532a32b4e712255eab99fd1c
Simon Funke <simon.funke@gmail.com>
2012-04-13 19:53:19 +0100

    Fill out the last remaining numbers

--- paper_functional_convergence/produce_plot.py
+++ paper_functional_convergence/produce_plot.py
@@ -19,18 +19,41 @@ def functional_value(x,y):
     values[ 101.,   34.] = 647.45568795
     values[ 101. ,   34.5] = 640.585912104
     values[ 101.,   35.] = 642.796161855
-    return values[x, y]/min(values.values())*100 - 100 # Relative change with respect to the minimum value
 
-# make these smaller to increase the resolution
+    values[ 101.5, 33. ] = 640.317213444
+    values[ 101.5, 34.5] = 638.095351195
+    values[ 101.5, 33.5] = 638.105521744
+    values[ 101.5, 34. ] = 644.927618316
+    values[ 101.5, 35. ] = 640.317213444
+
+    values[ 102., 34.5] = 645.291716549
+    values[ 102., 34.] = 652.143261284
+    values[ 102., 33.] = 647.586853698
+    values[ 102., 33.5] = 645.300152183
+    values[ 102., 35.] = 647.586853698
+
+    try:
+        return values[x, y]/min(values.values())*100 - 100 # Relative change with respect to the minimum value
+    except:
+        print "Returning fake value 0.0 for point ", x, y
+        return 0.0
+
+offset_x = 100
+offset_y = 33
 dx, dy = 0.5, 0.5
 
-x = arange(100, 101.000001, dx)
-y = arange(33, 35.000001, dy)
+x = arange(0., 2.000001, dx)
+y = arange(0., 2.000001, dy)
 X,Y = meshgrid(x, y)
 
-Z = numpy.vectorize(functional_value)(X, Y)
-pcolor(X, Y, Z)
-colorbar()
-axis([100,102,33,35])
+Z = numpy.vectorize(functional_value)(X + offset_x, Y + offset_y)
+# Interpolation: bicupic or nearest
+imshow(Z, interpolation='bicubic', cmap=cm.jet, origin='lower', extent=[0, 2, 0, 2])
+cbar = colorbar()
+cbar.set_label("Relative change in the power output")
+axis([0.,2.,0.,2.])
 
-show()
+xlabel("Offset x direction to reference position")
+ylabel("Offset y direction to reference position")
+
+savefig("functional_convergence.pdf")
