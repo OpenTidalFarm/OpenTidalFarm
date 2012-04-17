67b3ece2869a560ceee6950fb10c7d55ce35cf22
Simon Funke <simon.funke@gmail.com>
2012-04-17 11:48:46 +0100

    Support a user specified boundary value for the turbine position box constraints.

--- IPOptUtils.py
+++ IPOptUtils.py
@@ -23,14 +23,14 @@ class  IPOptFunction(object):
     ''' The Jacobian of the constraint functions evaluated at x. '''
     return (numpy.array([]), numpy.array([]))
 
-def position_constraints(params):
+def position_constraints(params, spacing_sides = 0):
   ''' This function returns the constraints to ensure that the turbine positions remain inside the domain. '''
   n = len(params["turbine_pos"])
   lc = []
   lb_x = params["turbine_x"]/2 
-  lb_y = params["turbine_y"]/2 
+  lb_y = params["turbine_y"]/2 + spacing_sides
   ub_x = params["basin_x"] - params["turbine_x"]/2 
-  ub_y = params["basin_y"] - params["turbine_y"]/2 
+  ub_y = params["basin_y"] - params["turbine_y"]/2 - spacing_sides
   
   # The control variable is ordered as [t1_x, t1_y, t2_x, t2_y, t3_x, ...]
   lb = n * [lb_x, lb_y]
