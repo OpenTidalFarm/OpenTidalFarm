9f96c1ffc9b3f9416d6b91d16becdd05ac7a5fe6
Simon Funke <simon.funke@gmail.com>
2012-04-17 12:29:10 +0100

    Revert "Clean examples and paper setups to keep a clean seperation between the code and the setups"
    This reverts commit e93df4e12eafaa2c9f99fcf3729a24c4d352e9b9.

---
+++ example_single_turbine_friction_vs_power_plot/sw.py
@@ -0,0 +1,43 @@
+''' An example that plots the power ouput of a single turbine for different fricition values. (see also test_optimal_friction_single_turbine) '''
+import sys
+import configuration 
+import numpy
+import matplotlib.pyplot as plt
+from dolfin import *
+from dolfin_adjoint import *
+from reduced_functional import ReducedFunctional
+
+# We set the perturbation_direction with a constant seed, so that it is consistent in a parallel environment.
+numpy.random.seed(21) 
+config = configuration.PaperConfiguration(nx = 40, ny = 20)
+config.params['dump_period'] = 1
+
+# Turbine settings
+config.params['turbine_pos'] = [[config.params['basin_x']/2, config.params['basin_y']/2]]
+config.params['turbine_friction'] = numpy.ones(len(config.params['turbine_pos']))
+config.params['controls'] = ['turbine_friction']
+config.params['finish_time'] = 1.5/4*config.period
+
+# Set up the model
+model = ReducedFunctional(config)
+m0 = model.initial_control()
+m_list = [m0*i for i in numpy.linspace(.1, 1., 10)]
+info_green('Testing friction coefficients: ' + str(m_list))
+
+# We already know that a zero friction leads to a zero power 
+P = [0]
+f = [0]
+for m in m_list: 
+  P.append(model.j(m, forward_only = True))
+  f.append(m[0])
+
+# Plot the results
+if MPI.process_number() == 0:
+  plt.figure(1)
+  plt.plot(f, P)
+  info_green('The maximum functional value of ' + str(max(P)) + ' is achieved with a friction coefficient of ' + str(f[numpy.argmax(P)]) + '.')
+  plt.ylabel('Power output')
+  plt.xlabel('Turbine coefficient')
+  plt.savefig('example_single_turbine_friction_vs_power_plot.pdf')
+  plt.show()
+  plt.hold()
---
+++ paper_plot_turbine_function/produce_plot.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+import matplotlib.pyplot as plt
+from matplotlib import cm
+from mpl_toolkits.mplot3d import Axes3D
+import numpy as np
+step = 0.04
+maxval = 1.0
+fig = plt.figure()
+ax = Axes3D(fig)
+
+# create supporting points in polar coordinates
+r = np.linspace(0, 20, 50)
+p = np.linspace(0, 20, 50)
+X,Y = np.meshgrid(r,p)
+
+Z1 = 1. - ((X-10)/10)**2
+Z1 = np.maximum(Z1, 1e-12)
+Z1 = np.exp(-1./Z1)
+
+Z2 = 1. - ((Y-10)/10)**2
+Z2 = np.maximum(Z2, 1e-12)
+Z2 = np.exp(-1./Z2)
+Z = Z1 * Z2
+
+ax.plot_surface(X, Y, Z, rstride=1, cstride=1, cmap=cm.jet)
+#ax.set_zlim3d(0, 1)
+#ax.set_xlabel(r'$\phi_\mathrm{real}$')
+#ax.set_ylabel(r'$\phi_\mathrm{im}$')
+#ax.set_zlabel(r'$V(\phi)$')
+plt.savefig('turbine_plot.pdf', facecolor='gray')
+plt.show()
