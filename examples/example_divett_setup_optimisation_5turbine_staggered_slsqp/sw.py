435ee8f04b8af4f318629653891e08556bbcdda0
Simon Funke <simon.funke@gmail.com>
2012-04-24 13:02:10 +0100

    Change the default friction to back 1.0 for the examples.

--- examples/example_divett_setup_optimisation_5turbine_staggered_slsqp/sw.py
+++ examples/example_divett_setup_optimisation_5turbine_staggered_slsqp/sw.py
@@ -4,20 +4,17 @@ import sys
 import configuration 
 import numpy
 import IPOptUtils
-from dirichlet_bc import DirichletBCSet
 from helpers import test_gradient_array
-from animated_plot import *
 from reduced_functional import ReducedFunctional
 from dolfin import *
 from scipy.optimize import fmin_slsqp
 set_log_level(ERROR)
 
-# An animated plot to visualise the development of the functional value
-plot = AnimatedPlot(xlabel='Iteration', ylabel='Functional value')
+parameters["std_out_all_processes"] = False
 
 # We set the perturbation_direction with a constant seed, so that it is consistent in a parallel environment.
 numpy.random.seed(21) 
-config = configuration.ConstantInflowPeriodicSidesPaperConfiguration(nx=100, ny=33)
+config = configuration.ConstantInflowPeriodicSidesPaperConfiguration()
 
 # The turbine position is the control variable 
 turbine_pos = [[60, 38], [80, 28], [100, 38], [120, 28], [140, 38]] 
@@ -25,7 +22,7 @@ turbine_pos = [[60, 38], [80, 28], [100, 38], [120, 28], [140, 38]]
 config.set_turbine_pos(turbine_pos)
 info_blue("Deployed " + str(len(turbine_pos)) + " turbines.")
 
-model = ReducedFunctional(config, scaling_factor = -10**-6, plot = True)
+model = ReducedFunctional(config, scaling_factor = -10**-1, plot = True)
 m0 = model.initial_control()
 
 g = lambda m: []
