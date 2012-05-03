ddbe8512c576540ce417de7d1988aef79ae2905c
Simon Funke <simon.funke@gmail.com>
2012-05-03 17:25:42 +0100

    Add a first version of scenario3

---
+++ examples/scenario3/mesh.geo
@@ -0,0 +1,30 @@
+basin_x = 1200;
+land_x = 600;
+land_y = 300;
+land_site_delta = 100;
+site_x = 150;
+site_y = 100;
+basin_y = land_y + basin_x - land_x;
+//element_size = 1;
+element_size = 10;
+element_size_coarse = 10;
+
+Point(1) = {0, 0, 0, element_size_coarse};
+Point(2) = {basin_x-land_x, 0, 0, element_size_coarse};
+Point(3) = {basin_x-land_x, land_y, 0, element_size_coarse};
+Point(4) = {basin_x, land_y, 0, element_size_coarse};
+Point(5) = {basin_x, basin_y, 0, element_size_coarse};
+Point(6) = {0, land_y, 0, element_size_coarse};
+Point(7) = {basin_x-land_x, basin_y, 0, element_size_coarse};
+
+Point(8) = {basin_x-land_x, land_y+land_site_delta, 0, element_size};
+Extrude{site_x, 0, 0} { Point{8}; Layers{site_x/element_size}; }
+Extrude{0, site_y, 0} { Line{1}; Layers{site_y/element_size}; }
+
+Line(6) = {1, 2};
+Line(7) = {2, 3};
+Line(8) = {3, 4};
+Line(9) = {4, 5};
+Line(10) = {1, 6};
+Line(11) = {5, 7};
+Circle(12) = {6, 3, 7};
---
+++ examples/scenario3/sw.py
@@ -0,0 +1,48 @@
+''' This example solves the forward model in an L shaped domain. '''
+import configuration 
+import numpy
+from dirichlet_bc import DirichletBCSet
+from reduced_functional import ReducedFunctional
+from domains import GMeshDomain 
+from dolfin import *
+set_log_level(PROGRESS)
+
+# We set the perturbation_direction with a constant seed, so that it is consistent in a parallel environment.
+numpy.random.seed(21) 
+config = configuration.ScenarioConfiguration("mesh.xml", inflow_direction = [1,1])
+
+# Place some turbines 
+basin_x = 1200
+basin_y = 1000
+land_x = 600
+land_y = 300
+land_site_delta = 100
+site_x = 150
+site_y = 100
+site_x_start = basin_x - land_x
+site_y_start = land_y + land_site_delta 
+config.params['turbine_x'] = config.params['turbine_y'] = 50. 
+
+turbine_pos = []
+for x_r in numpy.linspace(site_x_start, site_x_start + site_x, 2):
+    for y_r in numpy.linspace(site_y_start, site_y_start + site_y, 2):
+        turbine_pos.append((float(x_r), float(y_r)))
+config.set_turbine_pos(turbine_pos)
+
+info_blue("Deployed " + str(len(config.params["turbine_pos"])) + " turbines.")
+
+model = ReducedFunctional(config, scaling_factor = -10**-6)
+m0 = model.initial_control()
+j0 = model.j(m0)
+dj0 = model.dj(m0)
+info("Power outcome: %f" % (j0, ))
+info("Power gradient:" + str(dj0))
+info("Timing summary:")
+timer = Timer("NULL")
+timer.stop()
+print "Getting the adjoints a second time:"
+j0 = model.j(m0)
+dj0 = model.dj(m0)
+
+list_timings()
+
