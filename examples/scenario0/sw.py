57e4b212f3750d2ffa15a2c0a34e3c171837e403
Simon Funke <simon.funke@gmail.com>
2012-05-06 19:08:54 +0100

    A single turbine scenario

---
+++ examples/scenario0/sw.py
@@ -0,0 +1,22 @@
+import configuration 
+import numpy
+from reduced_functional import ReducedFunctional
+from dolfin import *
+set_log_level(ERROR)
+
+# We set the perturbation_direction with a constant seed, so that it is consistent in a parallel environment.
+config = configuration.ScenarioConfiguration("mesh.xml", inflow_direction = [1, 0])
+# Switch of the automatic scaling, since we will not solve an optimisation problem
+config.params['automatic_scaling'] = False
+
+# Place some turbines 
+basin_x = 640
+basin_y = 320
+turbine_pos = [[basin_x/2, basin_y/2]] 
+config.set_turbine_pos(turbine_pos)
+
+
+model = ReducedFunctional(config)
+m0 = model.initial_control()
+j0 = model.j(m0)
+info("Power outcome: %f" % (j0, ))
