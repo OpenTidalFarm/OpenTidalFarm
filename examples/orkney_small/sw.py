7c64389c0687f58b49fb5a0f7cb696fc60190390
Simon Funke <simon.funke@gmail.com>
2012-12-13 20:45:04 +0000

    add a small Orkney test

---
+++ examples/orkney_small/sw.py
@@ -0,0 +1,32 @@
+import configuration 
+import numpy
+import IPOptUtils
+from reduced_functional import ReducedFunctional
+from dolfin import *
+from scipy.optimize import fmin_slsqp
+set_log_level(ERROR)
+
+# We set the perturbation_direction with a constant seed, so that it is consistent in a parallel environment.
+numpy.random.seed(21) 
+
+# Some domain information extracted from the geo file
+site_x = 320.
+site_y = 160.
+site_x_start = 1.02957e+07 
+site_y_start = 6.52304e+06 - site_y 
+config = configuration.SinusoidalScenarioConfiguration("mesh/earth_orkney_converted.xml", inflow_direction = [1, 0])
+config.set_site_dimensions(site_x_start, site_x_start + site_x, site_y_start, site_y_start + site_y)
+
+# Place some turbines 
+IPOptUtils.deploy_turbines(config, nx = 8, ny = 4)
+
+model = ReducedFunctional(config, scaling_factor = -1, plot = True)
+m0 = model.initial_control()
+
+# Get the upper and lower bounds for the turbine positions
+lb, ub = IPOptUtils.position_constraints(config) 
+bounds = [(float(lb[i]), float(ub[i])) for i in range(len(lb))]
+
+f_ieqcons, fprime_ieqcons = IPOptUtils.get_minimum_distance_constraint_func(config)
+
+fmin_slsqp(model.j, m0, fprime = model.dj, bounds = bounds, f_ieqcons = f_ieqcons, fprime_ieqcons = fprime_ieqcons, iprint = 2, iter = 200)
