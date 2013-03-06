02ff2980b4f3930a38d3c2c03b9d7e60cce8d416
Simon Funke <simon.funke@gmail.com>
2013-03-06 19:13:53 +0000

    add a setup.py script for easy installation

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+setup(name='OpenTidalFarm',
+      version='0.2',
+      author='Simon Funke',
+      author_email='simon.funke@gmail.com',
+      url='http://www.opentidalfarm.com',
+      packages = ['opentidalfarm'] 
+      )
