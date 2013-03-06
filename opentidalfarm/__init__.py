209dfa726e8aa1f307bae7b92be4503a72515de1
Simon Funke <simon.funke@gmail.com>
2013-03-06 19:07:23 +0000

    restructure the code such that you can simply do a
    from opentidalfarm import *
    
    to import the necessary modules

---
+++ opentidalfarm/__init__.py
@@ -0,0 +1,16 @@
+"""
+
+The OpenTidalFarm project
+
+"""
+
+__version__ = '0.2'
+__author__  = 'Simon Funke'
+__credits__ = ['Simon Funke']
+__license__ = 'GPL-3'
+__maintainer__ = 'Simon Funke'
+__email__ = 'simon.funke@gmail.com'
+
+from configuration *
+from optimisation_helpers *
+
