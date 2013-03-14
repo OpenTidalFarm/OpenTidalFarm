7dfd4bcc8618a8ce1378651d98bbeaf2ed25f758
Simon Funke <simon.funke@gmail.com>
2013-03-14 19:51:45 +0000

    oops... add more missing files

---
+++ opentidalfarm/parameter_dict.py
@@ -0,0 +1,59 @@
+class ParameterDictionary(dict):
+    '''Parameter dictionary. This subclasses dict so defaults can be set.'''
+    def __init__(self, dict={}):
+        # Apply dict after defaults so as to overwrite the defaults
+        for key,val in dict.iteritems():
+            self[key]=val
+
+        self.required={
+            'verbose' : 'output verbosity',
+            'dt' : 'timestep',
+            'theta' : 'the implicitness for the time discretisation',
+            'start_time' : 'start time',
+            'current_time' : 'current time',
+            'finish_time' : 'finish time',
+            'steady_state' : 'steady state simulation',
+            'functional_final_time_only' : 'if the functional should be evaluated at the final time only (used if the time stepping is used to converge to a steady state)',
+            'dump_period' : 'dump period in timesteps; use 0 to deactivate disk outputs',
+            'bctype'  : 'type of boundary condition to be applied',
+            'strong_bc'  : 'list of strong dirichlet boundary conditions to be applied',
+            'free_slip_on_sides' : 'apply free slip boundary conditions on the sides (id=3)',
+            'initial_condition'  : 'initial condition function',
+            'include_advection': 'advection term on',
+            'include_diffusion': 'diffusion term on',
+            'diffusion_coef': 'diffusion coefficient',
+            'depth' : 'water depth at rest',
+            'g' : 'graviation',
+            'k' : 'wave length paramter. If you want a wave lenght of l, then set k to 2*pi/l.',
+            'eta0' : 'amplitude of free surface elevation',
+            'quadratic_friction' : 'quadratic friction',
+            'friction' : 'friction term on',
+            'turbine_pos' : 'list of turbine positions',
+            'turbine_x' : 'turbine extension in the x direction',
+            'turbine_y' : 'turbine extension in the y direction',
+            'turbine_friction' : 'turbine friction', 
+            'rho' : 'the density of the fluid', 
+            'controls' : 'the control variables',
+            'newton_solver': 'newton solver instead of a picard iteration',
+            'linear_solver' : 'default linear solver',
+            'preconditioner' : 'default preconditioner',
+            'picard_relative_tolerance': 'relative tolerance for the picard iteration',
+            'picard_iterations': 'maximum number of picard iterations',
+            'run_benchmark': 'benchmark to compare different solver/preconditioner combinations', 
+            'solver_exclude': 'solvers/preconditioners to be excluded from the benchmark',
+            'automatic_scaling': 'activates the initial automatic scaling of the functional',
+            'automatic_scaling_multiplier': 'defines the multiplier that determines the initial gradient length (= multiplier * turbine size)',
+            'print_individual_turbine_power': 'print out the power output of each individual turbine',
+            'save_checkpoints': 'automatically store checkpoints after each optimisation iteration'
+            }
+
+    def check(self):
+        # First check that no parameters are missing
+        for key, error in self.required.iteritems():
+            if not self.has_key(key):
+                raise KeyError, 'Missing parameter: ' + key + '. ' + 'This is used to set the ' + error + '.'
+        # Then check that no parameter is too much (as this is likely to be a mistake!)
+        diff = set(self.keys()) - set(self.required.keys())
+        if len(diff) > 0:
+            raise KeyError, 'Configuration has too many parameters: ' + str(diff)
+
