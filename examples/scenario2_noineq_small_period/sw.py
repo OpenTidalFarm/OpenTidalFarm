5a54badde793a788d709889b432acd3e13f8a7dc
Simon Funke <simon.funke@gmail.com>
2013-03-26 09:54:27 +0000

    Apply the correct initial and boundary conditions in scenario 2 and 4 after changing how these conditins are implemented in opentidalfarm

--- examples/scenario2_noineq_small_period/sw.py
+++ examples/scenario2_noineq_small_period/sw.py
@@ -8,8 +8,10 @@ site_x = 320.
 site_y = 160.
 site_x_start = (basin_x - site_x)/2 
 site_y_start = (basin_y - site_y)/2 
-config = UnsteadyConfiguration("mesh.xml", inflow_direction = [1,0], period = 10.*60)
+eta0 = 2.0/sqrt(9.81/50) # This will give a inflow velocity of 2m/s
+config = UnsteadyConfiguration("mesh.xml", inflow_direction = [1,0], period = 10.*60, eta0=eta0)
 config.set_site_dimensions(site_x_start, site_x_start + site_x, site_y_start, site_y_start + site_y)
+config.params['initial_condition'] = ConstantFlowInitialCondition(config)
 
 
 # Place some turbines 
@@ -17,7 +19,7 @@ deploy_turbines(config, nx = 8, ny = 4)
 
 config.info()
 
-rf = ReducedFunctional(config, plot = True)
+rf = ReducedFunctional(config)
 bounds = position_constraints(config) 
 
 maximize(rf, bounds=bounds, method = "SLSQP", options = {"maxiter": 400})
--- examples/scenario4_sin/sw.py
+++ examples/scenario4_sin/sw.py
@@ -9,15 +9,17 @@ site_y = 160.
 rad = 160.
 site_x_start = (basin_x - site_x)/2 
 site_y_start = (basin_y/2-rad)/2-site_y/2 
-config = UnsteadyConfiguration("mesh.xml", inflow_direction = [1, 0])
+eta0 = 2.0/sqrt(9.81/50) # This will give a inflow velocity of 2m/s
+config = UnsteadyConfiguration("mesh.xml", inflow_direction = [1, 0], eta0=eta0)
 config.set_site_dimensions(site_x_start, site_x_start + site_x, site_y_start, site_y_start + site_y)
+config.params['initial_condition'] = ConstantFlowInitialCondition(config) 
 
 # Place some turbines 
 deploy_turbines(config, nx = 8, ny = 4)
 
 config.info()
 
-rf = ReducedFunctional(config, plot = True)
+rf = ReducedFunctional(config)
 
 lb, ub = position_constraints(config) 
 ineq = get_minimum_distance_constraint_func(config)
