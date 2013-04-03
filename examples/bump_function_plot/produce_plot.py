e634e313dd8fb14e39c140bfe06b6a038748aff3
Simon Funke <simon.funke@gmail.com>
2013-04-03 17:00:08 +0100

    an improved version of the turbine bunp plot

--- examples/bump_function_plot/produce_plot.py
+++ examples/bump_function_plot/produce_plot.py
@@ -2,10 +2,12 @@
 import matplotlib.pyplot as plt
 from matplotlib import cm
 from mpl_toolkits.mplot3d import Axes3D
+from matplotlib import rc
 import numpy as np
 step = 0.04
 maxval = 1.0
 fig = plt.figure()
+fig.subplots_adjust(bottom=0.5, top=0.75)
 ax = Axes3D(fig)
 
 # create supporting points in polar coordinates
@@ -22,10 +24,16 @@ Z2 = np.maximum(Z2, 1e-12)
 Z2 = np.exp(-1./Z2)
 Z = Z1 * Z2 * np.exp(2)
 
-ax.plot_surface(X, Y, Z, rstride=1, cstride=1, cmap=cm.jet)
-#ax.set_zlim3d(0, 1)
-#ax.set_xlabel(r'$\phi_\mathrm{real}$')
-#ax.set_ylabel(r'$\phi_\mathrm{im}$')
-#ax.set_zlabel(r'$V(\phi)$')
-plt.savefig('turbine_plot.pdf', facecolor='gray')
-plt.show()
+ax.plot_surface(X, Y, Z, rstride=1, cstride=1, cmap=cm.jet, linewidth=0.3)
+plt.setp(ax.get_zticklabels(), fontsize=20)
+plt.setp(ax.get_xticklabels(), fontsize=20)  
+plt.setp(ax.get_yticklabels(), fontsize=20)
+
+ax.set_xmargin(0)
+ax.set_ymargin(0)
+#ax.grid(b=None)
+rc('text', usetex = True)
+scaling = 1.7
+plt.figure(1, figsize = (scaling*7., scaling*4.))
+
+plt.savefig('turbine_plot.pdf', facecolor='white',bbox_inches='tight')
