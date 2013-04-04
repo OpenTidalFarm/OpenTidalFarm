82833cf62a3d947f3a7406c68793ee6ce6f030ff
Simon Funke <simon.funke@gmail.com>
2013-04-04 18:03:03 +0100

    multrigrid implementation is ready to go.

--- examples/scenario1_multigrid/sw.py
+++ examples/scenario1_multigrid/sw.py
@@ -3,7 +3,13 @@ from optparse import OptionParser
 import sys
 set_log_level(INFO)
 
-meshfile = sys.argv[1] 
+layer = int(sys.argv[1])
+# Extract the settings that we use for this multigrid layer
+#            Meshfile      Max iter           
+settings = (["mesh0.xml",        50],
+            ["mesh1.xml",        40],
+            ["mesh2.xml",        30])[layer]
+
 # Some domain information extracted from the geo file
 basin_x = 640.
 basin_y = 320.
@@ -11,7 +17,7 @@ site_x = 320.
 site_y = 160.
 site_x_start = (basin_x - site_x)/2 
 site_y_start = (basin_y - site_y)/2 
-config = SteadyConfiguration(meshfile, inflow_direction = [1, 0])
+config = SteadyConfiguration(settings[0], inflow_direction = [1, 0])
 config.set_site_dimensions(site_x_start, site_x_start + site_x, site_y_start, site_y_start + site_y)
 config.params['save_checkpoints'] = True
 
@@ -29,5 +35,4 @@ except IOError:
 	print "No checkpoints file found."
 
 lb, ub = position_constraints(config) 
-ineq = get_minimum_distance_constraint_func(config)
-maximize(rf, bounds = [lb, ub], constraints = ineq, method = "SLSQP", options = {"maxiter": 200})
+maximize(rf, bounds = [lb, ub], method = "SLSQP", options = {"maxiter": settings[1]})
