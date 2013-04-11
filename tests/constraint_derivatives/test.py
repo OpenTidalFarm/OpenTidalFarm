c9d83ec413f809b11884f3d60d6fb883daa175ea
Simon Funke <simon.funke@gmail.com>
2013-04-11 14:25:39 +0100

    add a test that checks that the gradients of the general site inequality constraints are correct

--- tests/constraint_derivatives/test.py
+++ tests/constraint_derivatives/test.py
@@ -19,8 +19,10 @@ info_green("Test passed")
 ieq = generate_site_constraints(config, [[10, 10], [20, 10], [20, 20], [10, 20]])
 ieqcons_J = lambda m: ieq['fun'](m)[0]
 ieqcons_dJ = lambda m, forget=False: ieq['jac'](m)[0]
-minconv = helpers.test_gradient_array(ieqcons_J, ieqcons_dJ, numpy.array([1., 2., 3., 4., 7., 1., 6., 9.]))
-if minconv < 1.99:
+minconv = helpers.test_gradient_array(ieqcons_J, ieqcons_dJ, numpy.array([1., 2., 3., 4.]), seed=0.001)
+# These constraints are linear so we expect no convergence at all.
+# Let's check that the tolerance is not above a threshold
+if minconv > 0.5:
     info_red("Convergence for the polygon shaped sites failed")
     sys.exit(1)
 info_green("Test passed")    
