ef3b16908ec591239344758a42c9783791a09193
Simon Funke <simon.funke@gmail.com>
2013-04-25 12:16:43 +0100

    fix the power_curve example

--- examples/power_curve/sw.py
+++ examples/power_curve/sw.py
@@ -2,7 +2,7 @@
 from opentidalfarm import *
 import matplotlib.pyplot as plt
 import numpy
-set_log_level(PROGRESS)
+set_log_level(INFO)
 parameters['form_compiler']['cpp_optimize_flags'] = '-O3 -fno-math-errno -march=native'        
 parameters['form_compiler']['quadrature_degree'] = 20
 
@@ -13,6 +13,8 @@ inflow_direction = [1, 0]
 
 config = SteadyConfiguration("mesh.xml", inflow_direction=inflow_direction)
 config.functional = PowerCurveFunctional
+config.params['turbine_thrust_parametrisation'] = True
+config.params['initial_condition'] = ConstantFlowInitialCondition(config)
 config.params['automatic_scaling'] = False
 
 # Place one turbine 
