db860ad2012f8f45e1700be9ffe1bc94dd741cde
Simon Funke <simon.funke@gmail.com>
2013-06-07 10:59:02 +0100

    Make convert_to_new_xml.py such that it also converts physical regions.

--- scripts/convert_to_new_xml.py
+++ scripts/convert_to_new_xml.py
@@ -9,3 +9,10 @@ mesh = Mesh(meshfile)
 mesh_function = MeshFunction('size_t', mesh, meshfile[:-4] + "_facet_region.xml")
 file = File(meshfile[:-4] + '_facet_region.xml')
 file << mesh_function
+
+try:
+	mesh_function = MeshFunction('size_t', mesh, meshfile[:-4] + "_physical_region.xml")
+	file = File(meshfile[:-4] + '_physical_region.xml')
+	file << mesh_function
+except IOError:
+	print "Could not find a physical region file. Skipping."
