9280b30ca0fe8701981a89ab3c74f988e4714e8f
Simon Funke <simon.funke@gmail.com>
2013-07-31 18:36:05 +0100

    Replace the hasing strategy used for the memoisation (which was pickle.dumps) with a safer choice (convert data structures to tuples)

--- opentidalfarm/memoize.py
+++ opentidalfarm/memoize.py
@@ -2,27 +2,42 @@ import numpy
 import cPickle
 from helpers import cpu0only
 
+def to_tuple(obj):
+    if hasattr(obj, '__iter__'): 
+        return tuple([to_tuple(o) for o in obj])
+    else:
+        return obj 
+
 # Implement a memoization function to avoid duplicated functional (derivative) evaluations
 class MemoizeMutable:
+
+    def get_key(self, args, kwds):
+        h1 = to_tuple(args)
+        h2 = to_tuple(kwds.items())
+        h = tuple([h1, h2])
+        # h = hash(h)  # We could hash it, but it is often useful to have the 
+                       # explicit turbine parameter -> functional value mapping
+        return h
 
     def __init__(self, fn):
         self.fn = fn
         self.memo = {}
 
     def __call__(self, *args, **kwds):
-        str = cPickle.dumps(args, 1)+cPickle.dumps(kwds, 1)
-        if not self.memo.has_key(str): 
-            self.memo[str] = self.fn(*args, **kwds)
-        return self.memo[str]
+        h = self.get_key(args, kwds)
+
+        if not self.memo.has_key(h): 
+            self.memo[h] = self.fn(*args, **kwds)
+        return self.memo[h]
 
     def has_cache(self, *args, **kwds):
-        str = cPickle.dumps(args, 1)+cPickle.dumps(kwds, 1)
-        return str in self.memo
+        h = self.get_key(args, kwds)
+        return h in self.memo
 
     # Insert a function value into the cache manually.
     def __add__(self, value, *args, **kwds):
-        str = cPickle.dumps(args, 1)+cPickle.dumps(kwds, 1)
-        self.memo[str] = value
+        h = self.get_key(args, kwds)
+        self.memo[h] = value
 
     @cpu0only
     def save_checkpoint(self, filename):
