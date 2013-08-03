737482e7558ce82e8b0c1942e0650b55da22b500
Simon Funke <simon.funke@gmail.com>
2013-08-03 22:13:22 +0100

    More plotting stuff

--- examples/scenario2_noineq_small_period/plot_power_timeline.py
+++ examples/scenario2_noineq_small_period/plot_power_timeline.py
@@ -11,20 +11,22 @@ for i in funcs_dupl:
     if i not in func:
         func.append(i)
 
-it = [float(i)/5 for i in range(len(func))]
+period = 10
+it = [float(i)/len(func)*period for i in range(len(func))]
 func = numpy.array(func)*1e-6
 
 filename = "power_timeline.pdf"
-scaling = 0.7
+scaling = 0.5
 rc('text', usetex = True)
 plt.figure(1, figsize = (scaling*7., scaling*4.))
-plt.gcf().subplots_adjust(bottom=0.15)
+plt.gcf().subplots_adjust(bottom=0.2)
+plt.gcf().subplots_adjust(left=0.15)
 plt.plot(it, func, color = 'black')
 #plt.yscale('log')
 #plt.axis([0, times[-1], -2.5, 2.5])
 #plt.xticks(numpy.arange(0, times[-1]+1, 5))
-#plt.yticks(numpy.arange(14, basin_x_total/1000, 2))
-plt.ylabel(r"Power production [MW]")
+plt.yticks(numpy.arange(0, 50, 10))
+plt.ylabel(r"Power [MW]")
 plt.xlabel(r"Time [min]")
 plt.savefig(filename)
 plt.close()
