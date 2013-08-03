f2dfee5716763f3eb7b65ba958364ee6ee81b6f7
Simon Funke <simon.funke@gmail.com>
2013-08-03 16:33:10 +0100

    fix the optimal_friction_mini_model test

--- tests/optimal_friction_mini_model/sw.py
+++ tests/optimal_friction_mini_model/sw.py
@@ -2,7 +2,7 @@
  - single turbine (with constant friction distribution) whose size exceeds the size of the domain
  - constant velocity profile with an initial x-velocity of 2.
  - control: turbine friction
- - the mini model will compute a x-velocity of 2/(f + 1) wher ef is the turbine friction.
+ - the mini model will compute a x-velocity of 2/(f + 1) where f is the turbine friction.
  - the functional is \int C * f * ||u||**3 where C is a constant
  - hence we maximise C * f * ( 2/(f + 1) )**3, f > 0 which has the solution f = 0.5
 
@@ -16,7 +16,7 @@ from opentidalfarm import *
 set_log_level(PROGRESS)
 
 def default_config():
-  config = configuration.DefaultConfiguration(nx=20, ny=10, finite_element = finite_elements.p1dgp2)
+  config = configuration.DefaultConfiguration(nx=20, ny=10, finite_element=finite_elements.p1dgp2)
   config.params["verbose"] = 0
 
   # dt is used in the functional only, so we set it here to 1.0
@@ -25,8 +25,8 @@ def default_config():
   config.params["turbine_pos"] = [[500., 500.]]
   # The turbine friction is the control variable 
   config.params["turbine_friction"] = 12.0*numpy.random.rand(len(config.params["turbine_pos"]))
-  config.params["turbine_x"] = 8000
-  config.params["turbine_y"] = 8000
+  config.params["turbine_x"] = 1e10
+  config.params["turbine_y"] = 1e10
   config.params['controls'] = ['turbine_friction']
   config.params["functional_final_time_only"] = True
 
@@ -36,8 +36,10 @@ def default_config():
   return config
 
 config = default_config()
-rf = ReducedFunctional(config, scale = 1e-3, forward_model = mini_model.mini_model_solve)
+rf = ReducedFunctional(config, scale = 1e-3, forward_model=mini_model.mini_model_solve)
 m0 = rf.initial_control()
+rf(m0)
+rf.dj(m0, forget=False)
 
 p = numpy.random.rand(len(m0))
 minconv = helpers.test_gradient_array(rf.j, rf.dj, m0, seed=0.001, perturbation_direction=p)
@@ -46,10 +48,10 @@ if minconv < 1.98:
   sys.exit(1)
 
 bounds = [0, 100]
-m = maximize(rf, bounds = bounds, method = "SLSQP", scale=1e-3) 
+maximize(rf, bounds=bounds, method="SLSQP", scale=1e-3) 
 
-if abs(m[0]-0.61216779034) > 10**-4: 
-  info_red("The optimisation algorithm did not find the correct solution.")
+if abs(config.params["turbine_friction"][0]-0.5) > 10**-4: 
+  info_red("The optimisation algorithm did not find the optimal friction value.")
   sys.exit(1) 
 else:
   info_green("Test passed")
