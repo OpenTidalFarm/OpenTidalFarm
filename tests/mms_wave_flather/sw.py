60868db1d83076c2cba31392261c39181cc423cf
Patrick Farrell <patrick.farrell@imperial.ac.uk>
2013-11-06 15:29:24 +0000

    Fix the mms_wave_flather test.

--- tests/mms_wave_flather/sw.py
+++ tests/mms_wave_flather/sw.py
@@ -1,6 +1,7 @@
 import sys
 from opentidalfarm import *
 from opentidalfarm.initial_conditions import SinusoidalInitialCondition
+import opentidalfarm.domains
 from dolfin_adjoint import adj_reset 
 from math import log
 
@@ -24,10 +25,12 @@ def error(config, eta0, k):
   e = state - exactstate
   return sqrt(assemble(dot(e,e)*dx))
 
-def test(refinment_level):
-  config = DefaultConfiguration(nx=2*2**refinment_level, ny=2, finite_element=finite_elements.p1dgp2) 
+def test(refinement_level):
+  config = DefaultConfiguration(nx=2*2**refinement_level, ny=2, finite_element=finite_elements.p1dgp2) 
+  config.set_domain(opentidalfarm.domains.RectangularDomain(3000, 1000, 2*2**refinement_level, 2))
   eta0 = 2.0
   k = pi/config.domain.basin_x
+  config.params['k'] = k
   config.params["finish_time"] = pi/(sqrt(config.params["g"]*config.params["depth"])*k)/10
   config.params["dt"] = config.params["finish_time"]/150
   config.params["dump_period"] = 100000
@@ -36,8 +39,8 @@ def test(refinment_level):
 
 errors = []
 tests = 5
-for refinment_level in range(1, tests):
-  errors.append(test(refinment_level))
+for refinement_level in range(1, tests):
+  errors.append(test(refinement_level))
 # Compute the order of convergence 
 conv = [] 
 for i in range(len(errors)-1):
--- tests/mms_wave_flather/sw_time.py
+++ tests/mms_wave_flather/sw_time.py
@@ -1,6 +1,7 @@
 import sys
 from opentidalfarm import *
 from opentidalfarm.initial_conditions import SinusoidalInitialCondition
+import opentidalfarm.domains
 from dolfin_adjoint import adj_reset 
 from math import log
 
@@ -24,12 +25,14 @@ def error(config, eta0, k):
   e = state - exactstate
   return sqrt(assemble(dot(e,e)*dx))
 
-def test(refinment_level):
+def test(refinement_level):
   config = DefaultConfiguration(nx=2**8, ny=2, finite_element = finite_elements.p1dgp2) 
+  config.set_domain(opentidalfarm.domains.RectangularDomain(3000, 1000, 2**8, 2))
   eta0 = 2.0
   k = pi/config.domain.basin_x
+  config.params['k'] = k
   config.params["finish_time"] = pi/(sqrt(config.params["g"]*config.params["depth"])*k)
-  config.params["dt"] = config.params["finish_time"]/(2*2**refinment_level)
+  config.params["dt"] = config.params["finish_time"]/(2*2**refinement_level)
   config.params["theta"] = 0.5
   config.params["dump_period"]=100000
 
@@ -37,8 +40,8 @@ def test(refinment_level):
 
 errors = []
 tests = 6
-for refinment_level in range(1, tests):
-  errors.append(test(refinment_level))
+for refinement_level in range(1, tests):
+  errors.append(test(refinement_level))
 # Compute the order of convergence 
 conv = [] 
 for i in range(len(errors)-1):
