1f39c3ea9d9072ff92c3c97556d229ab579d70d6
Simon Funke <simon.funke@gmail.com>
2013-11-24 19:15:30 +0100

    ...

--- examples/orkney_farm_optimisation/sw_multisteady.py
+++ examples/orkney_farm_optimisation/sw_multisteady.py
@@ -1,14 +1,11 @@
 from opentidalfarm import *
-import utm
-import uptide
-import uptide.tidal_netcdf
-from uptide.netcdf_reader import NetCDFInterpolator
+from common import TidalForcing, BathymetryDepthExpression
 from math import pi
-import datetime
 import os.path
 utm_zone = 30
 utm_band = 'V'
 forward_only = False
+test_gradient = False
 
 config = UnsteadyConfiguration("mesh/coast_idBoundary_utm.xml", [1, 1]) 
 config.params['initial_condition'] = ConstantFlowInitialCondition(config) 
@@ -17,6 +14,7 @@ config.params["controls"] = ["turbine_friction"]
 config.params["turbine_parametrisation"] = "smooth"
 config.params["automatic_scaling"] = False 
 config.params['friction'] = Constant(0.0025)
+config.params['cache_forward_state'] = True 
 config.params['base_path'] = "results_multisteady"
 
 # Perform only two timesteps
@@ -29,25 +27,6 @@ config.params['functional_quadrature_degree'] = 0
 
 # Tidal boundary forcing
 bc = DirichletBCSet(config)
-class TidalForcing(Expression):
-    def __init__(self):
-        self.t = None
-        self.tnci_time = None
-
-        constituents = ['Q1', 'O1', 'P1', 'K1', 'N2', 'M2', 'S2', 'K2']
-        tide = uptide.Tides(constituents)
-        tide.set_initial_time(datetime.datetime(2001,9,18,0,0,0))
-        self.tnci = uptide.tidal_netcdf.OTPSncTidalInterpolator(tide,
-                    'gridES2008.nc', 'hf.ES2008.nc', ranges=((-4.0,0.0),(58.0,61.0)))
-
-    def eval(self, values, X):
-        if self.tnci_time != self.t:    
-            self.tnci.set_time(self.t)
-            self.tnci_time = self.t
-
-        latlon = utm.to_latlon(X[0], X[1], utm_zone, utm_band)
-        # OTPS has lon, lat coordinates!
-        values[0] = self.tnci.get_val((latlon[1], latlon[0]), allow_extrapolation=True)
 
 eta_expr = TidalForcing() 
 bc.add_analytic_eta(1, eta_expr)
@@ -57,19 +36,10 @@ bc.add_noslip_u(3)
 config.params['bctype'] = 'strong_dirichlet'
 config.params['strong_bc'] = bc
 
-# Bathymetry
-class BathymetryDepthExpression(Expression):
-  def __init__(self, filename):
-    self.nci = NetCDFInterpolator(filename, ('lat', 'lon'), ('lat', 'lon'))
-    self.nci.set_field("z")
-
-  def eval(self, values, x):
-    latlon = utm.to_latlon(x[0], x[1], utm_zone, utm_band)
-    values[0] = max(10, -self.nci.get_val(latlon))
-
 V_cg1 = FunctionSpace(config.domain.mesh, "CG", 1)
 V_dg0 = FunctionSpace(config.domain.mesh, 'DG', 0)
 
+# Bathymetry
 bexpr = BathymetryDepthExpression('bathymetry.nc')
 depth = interpolate(bexpr, V_cg1) 
 depth_pvd = File(os.path.join(config.params["base_path"], "bathymetry.pvd"))
@@ -90,11 +60,13 @@ config.info()
 rf = ReducedFunctional(config, scale=-1e-6)
 rf.load_checkpoint()
 
-if forward_only:
+if forward_only or test_gradient:
     print "Running forward model"
     m0 = rf.initial_control()
-    j = rf.j(m0, annotate=False)
+    j = rf.j(m0, annotate=test_gradient)
     print "Power: ", j
+    if test_gradient:
+        dj = rf.dj_with_check(m0, forget=False, seed=0.01)
 
 else:
   # The maximum friction is given by:
