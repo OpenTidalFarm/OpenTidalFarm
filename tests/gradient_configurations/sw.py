d99279bc82201dcbe368622975d029d21b241e59
Simon Funke <simon.funke@gmail.com>
2013-11-24 21:16:32 +0000

    Don't try and plot stuff in tests.

--- tests/gradient_configurations/sw.py
+++ tests/gradient_configurations/sw.py
@@ -58,7 +58,8 @@ for c in [DefaultConfiguration, SteadyConfiguration]:
 
     p = numpy.random.rand(len(m0))
     seed = 0.1
-    minconv = helpers.test_gradient_array(model.j, model.dj, m0, seed = seed, perturbation_direction = p, plot_file = "convergence_" + c.__name__ + ".pdf")
+    #minconv = helpers.test_gradient_array(model.j, model.dj, m0, seed = seed, perturbation_direction = p, plot_file = "convergence_" + c.__name__ + ".pdf")
+    minconv = helpers.test_gradient_array(model.j, model.dj, m0, seed = seed, perturbation_direction = p)
     if minconv < 1.9:
         info_red("The gradient taylor remainder test failed for the " + c.__name__ + " configuration.")
         sys.exit(1)
