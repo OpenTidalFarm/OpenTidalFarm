41d19804810f3ddc87c992fddc3de8104857f213
Simon Funke <simon.funke@gmail.com>
2014-03-03 11:03:12 +0100

    Add command line arguments to the iter_plot scripts
    Also check that the input files are valid.

--- scripts/iter_plot.py
+++ scripts/iter_plot.py
@@ -2,13 +2,22 @@
 import sys
 import re
 import matplotlib.pyplot as plt
+import argparse
 from matplotlib import rc
-filename = sys.argv[1]
-f = open(filename, "r")
+
+parser = argparse.ArgumentParser(description='Create a convergence plot from OpenTidalFarm stdout file.')
+parser.add_argument('file', type=str, help='the filename containing the OpenTidalFarm stdout')
+parser.add_argument('--scale', default=1., type=float, help='rescaling factor (in addition to the MW conversion, i.e. division by 1e-6).')
+parser.add_argument('--output', default='iter_plot.pdf', type=str, help='output filename')
+
+args = parser.parse_args()
+
+f = open(args.file, "r")
 
 # Read the output file of the optimisation run and parse it
 # This script assumes that scipy.slsqp was used.
 
+found_slsqp = False
 # The iteration numbers
 it = []
 last_it = 0
@@ -19,8 +28,11 @@ func_evals = []
 finished = False
 # Rescale the  functional values before plotting
 # Note: this does not yet include the automatic scaling performed by the turbine code. We will recover that factor from the output file
-rescale = 10**-6
+rescale = 10**-6 * args.scale
 for line in f:
+    if "  NIT    FC           OBJFUN            GNORM" in line:
+        found_slsqp = True
+
     if "Optimization terminated successfully" in line:
 	finished = True
     elif "The automatic scaling factor was set to" in line:
@@ -35,7 +47,7 @@ for line in f:
 	    print line,
     # The sqp iterations output are characteristic as they consists of four numers and do not have any commas in the output
     elif "," not in line:
-	m = re.match(r".* ([0-9]+) \s+ ([0-9]+) \s+ ([0-9|\.|E\+\-]+) \s+ ([0-9|\.|E\+\-]+)", line) # Find the SLSQP lines, mainly by counting the the spaces between the numbesr
+	m = re.match(r".* ([0-9]+) \s+ ([0-9]+) \s+ ([0-9|\.|E\+\-]+) \s+ ([0-9|\.|E\+\-]+)", line) # Find the SLSQP lines, mainly by counting the the spaces between the numbers
 	# re.match(r"\s+ ([0-9]+) \s+ ([0-9]+) \s+ ([0-9|\.|E\+\-]+) \s+ ([0-9|\.|E\+\-]+)", line)
         try:
 	    if len(it) == 0 or it[-1] != int(m.group(1)):
@@ -49,12 +61,19 @@ for line in f:
 	    pass
 f.close()
 
+if not found_slsqp:
+    print "No L-BFGS-B output found. Please supply the stdout record of an OpenTidalFarm simulation which used the L-BFGS-B optimisation algorithm."
+    sys.exit(1)
+
 print "Power output of initial layout: ", func[0]
 print "Power output of final layout: ", func[-1]
-print "Relative power increase: ", func[-1]/func[0]
+try:
+    print "Relative power increase: ", func[-1]/func[0]
+except ZeroDivisionError:
+    print "Relative power increase: oo"
+print "Number of iterations: ", len(func)
 
-# The produce a nice plot
-filename = "iter_plot.pdf"
+# Produce plot
 scaling = 0.7
 rc('text', usetex = True)
 plt.figure(1, figsize = (scaling*7., scaling*4.))
@@ -66,5 +85,5 @@ plt.plot(it, func, color = 'black')
 #plt.yticks(numpy.arange(14, basin_x_total/1000, 2))
 plt.ylabel(r"Power production [MW]")
 plt.xlabel(r"Optimisation iteration")
-plt.savefig(filename)
+plt.savefig(args.output)
 plt.close()
--- scripts/iter_plot_lbfgsb.py
+++ scripts/iter_plot_lbfgsb.py
@@ -2,13 +2,22 @@
 import sys
 import re
 import matplotlib.pyplot as plt
+import argparse
 from matplotlib import rc
-filename = sys.argv[1]
-f = open(filename, "r")
+
+parser = argparse.ArgumentParser(description='Create a convergence plot from OpenTidalFarm stdout file.')
+parser.add_argument('file', type=str, help='the filename containing the OpenTidalFarm stdout')
+parser.add_argument('--scale', default=1., type=float, help='rescaling factor (in addition to the MW conversion, i.e. division by 1e-6).')
+parser.add_argument('--output', default='iter_plot.pdf', type=str, help='output filename')
+
+args = parser.parse_args()
+
+f = open(args.file, "r")
 
 # Read the output file of the optimisation run and parse it
 # This script assumes that the scipy.L-BFGS-B algorithm was used.
 
+found_lbfgsb = False
 # The iteration numbers
 it = []
 last_it = 0
@@ -17,8 +26,11 @@ func = []
 finished = False
 # Rescale the  functional values before plotting
 # Note: this does not yet include the automatic scaling performed by the turbine code. We will recover that factor from the output file
-rescale = 10**-6
+rescale = 10**-6 * args.scale
 for line in f:
+    if "RUNNING THE L-BFGS-B CODE" in line:
+        found_lbfgsb = True
+
     if "Optimization terminated successfully" in line:
         finished = True
     elif "The automatic scaling factor was set to" in line:
@@ -32,12 +44,19 @@ for line in f:
 
 f.close()
 
-print "Power output of initial layout: ", func[0]
-print "Power output of initial layout: ", func[-1]
-print "Relative power increase: ", func[-1]/func[0]
+if not found_lbfgsb:
+    print "No L-BFGS-B output found. Please supply the stdout record of an OpenTidalFarm simulation which used the L-BFGS-B optimisation algorithm."
+    sys.exit(1)
 
-# The produce a nice plot
-filename = "iter_plot.pdf"
+print "Power output of initial layout: ", func[0]
+print "Power output of final layout: ", func[-1]
+try:
+    print "Relative power increase: ", func[-1]/func[0]
+except ZeroDivisionError:
+    print "Relative power increase: oo"
+print "Number of iterations: ", len(func)
+
+# Produce plot
 scaling = 0.5
 rc('text', usetex = True)
 plt.figure(1, figsize = (scaling*7., scaling*4.))
@@ -48,7 +67,7 @@ plt.plot(it, func, color = 'black')
 #plt.axis([0, times[-1], -2.5, 2.5])
 #plt.xticks(numpy.arange(0, times[-1]+1, 5))
 #plt.yticks(numpy.arange(14, basin_x_total/1000, 2))
-plt.ylabel(r"Average power [MW]")
+plt.ylabel(r"Power production [MW]")
 plt.xlabel(r"Optimisation iteration")
-plt.savefig(filename)
+plt.savefig(args.output)
 plt.close()
