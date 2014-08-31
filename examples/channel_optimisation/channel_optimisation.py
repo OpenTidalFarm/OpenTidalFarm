885e162f6af43f68aa28c3e61423c70bd236ff7d
Simon Funke <simon.funke@gmail.com>
2014-08-31 22:22:46 +0200

    Rename files

---
+++ examples/channel_optimisation/channel_optimisation.py
@@ -0,0 +1,23 @@
+from opentidalfarm import *
+set_log_level(INFO)
+
+# Some domain information extracted from the geo file
+basin_x = 640.
+basin_y = 320.
+site_x = 320.
+site_y = 160.
+site_x_start = (basin_x - site_x)/2 
+site_y_start = (basin_y - site_y)/2 
+config = SteadyConfiguration("mesh_coarse.xml", inflow_direction = [1, 0])
+config.set_site_dimensions(site_x_start, site_x_start + site_x, site_y_start, site_y_start + site_y)
+
+# Place some turbines 
+deploy_turbines(config, nx = 8, ny = 4)
+
+config.info()
+
+rf = ReducedFunctional(config)
+
+lb, ub = position_constraints(config) 
+ineq = get_minimum_distance_constraint_func(config)
+maximize(rf, bounds = [lb, ub], constraints = ineq, method = "SLSQP", options={'maxiter':3})
