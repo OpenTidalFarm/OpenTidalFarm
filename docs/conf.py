e66ba826a08f6dc54175735cd46fc470961b88d2
Simon Funke <simon.funke@gmail.com>
2014-09-12 21:25:49 +0200

    Add viewcode extension

--- docs/conf.py
+++ docs/conf.py
@@ -74,6 +74,7 @@ sys.path.insert(0, os.path.abspath('..'))
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.mathjax',
+    'sphinx.ext.viewcode',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
