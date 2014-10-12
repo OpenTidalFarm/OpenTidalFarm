dd2c08546334cc90d757ceb24eff8e9416c01fb9
George Barnett <george.barnett10@imperial.ac.uk>
2014-10-12 10:46:52 +0000

    Update channel-optimization for new interface

--- examples/channel-optimization/channel-optimization.py
+++ examples/channel-optimization/channel-optimization.py
@@ -25,10 +25,6 @@ from opentidalfarm import *
 # Create a rectangular domain.
 domain = FileDomain("mesh/mesh.xml")
 
-import IPython
-IPython.embed()
-
-
 # Specify boundary conditions.
 bcs = BoundaryConditionSet()
 bcs.add_bc("u", Constant((2, 0)), facet_id=1)
@@ -48,18 +44,15 @@ prob_params.friction = Constant(0.0025)
 # farm consists of 32 turbines, initially deployed in a regular grid layout.
 # This layout will be the starting guess for the optimization.
 
-# A rectangular farm is defined using the domain and the site dimensions.
-farm = RectangularFarm(domain,
-                       site_x_start=160, site_x_end=480,
-                       site_y_start=80, site_y_end=240)
+# Before adding turbines we must specify the type of turbines used in the array.
+# Here we used the default BumpTurbine which defaults to being controlled by
+# just position. The diameter and friction are set. The minimum distance between
+# turbines if not specified is set to 1.5*diameter.
+turbine = BumpTurbine(diameter=20.0, friction=12.0)
 
-# Before adding turbines we must specify the type of turbines used in the array,
-# their controls and their parameterisation.
-controls = Controls(position=True)
-parameterisation = TurbineParameterisation()
-farm.turbine_prototype = Turbine(diameter=20.0, minimum_distance=40.0,
-                                 maximum_friction=21.0, controls=controls,
-                                 parameterisation=parameterisation)
+# A rectangular farm is defined using the domain and the site dimensions.
+farm = RectangularFarm(domain, site_x_start=160, site_x_end=480,
+                       site_y_start=80, site_y_end=240, turbine=turbine)
 
 # Turbines are then added to the site in a regular grid layout.
 farm.add_regular_turbine_layout(num_x=8, num_y=4)
