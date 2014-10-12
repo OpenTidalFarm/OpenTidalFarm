f5c7aafe5feabb587648bcebb29babe57cb125bc
George Barnett <george.barnett10@imperial.ac.uk>
2014-10-12 10:57:30 +0000

    Update test_minimal_distance_constraint.py -- PASSES

--- tests/minimal_distance_constraint/test_minimal_distance_constraint.py
+++ tests/minimal_distance_constraint/test_minimal_distance_constraint.py
@@ -8,33 +8,36 @@ import math
 class TestMinimalDistanceConstraint(object):
 
     def test_derivative(self):
+
+        turbine = BumpTurbine(diameter=10.0, friction=1.0)
         domain = RectangularDomain(0, 0, 3000, 1000, 20, 3)
-        farm = TidalFarm(domain)
+        farm = Farm(domain, turbine)
+        farm.add_turbine((1500, 500))
 
-        farm.params["controls"] = ['turbine_pos']
-
-        ieq = get_minimum_distance_constraint_func(farm)
+        ieq = farm.minimum_distance_constraints()
 
         # Only test the correctness of the first inequality constraint for simplicity
         ieqcons_J = lambda m: ieq.function(m)[0]
         ieqcons_dJ = lambda m, forget=False: ieq.jacobian(m)[0]
 
-        minconv = helpers.test_gradient_array(ieqcons_J, ieqcons_dJ, numpy.array([1., 2., 3., 4., 7., 1., 6., 9.]))
+        x = numpy.array([1., 2., 3., 4., 7., 1., 6., 9.])
+        minconv = helpers.test_gradient_array(ieqcons_J, ieqcons_dJ, x)
 
         assert minconv > 1.99
 
-        ieq = generate_site_constraints(farm, [[0, 0], [10, 0], [10, 10]], penalty_factor=1)
+        ieq = generate_site_constraints(farm, [[0, 0], [10, 0], [10, 10]],
+                                        penalty_factor=1)
 
-        # Only test the correctness of the first inequality constraint for simplicity
+        # Only test the correctness of the first inequality constraint for
+        # simplicity.
         ieqcons_J = lambda m: ieq.function(m)[0]
         ieqcons_dJ = lambda m, forget=False: ieq.jacobian(m)[0]
 
         # This will raise a RunetimeWarning, which is fine because we expect
         # division by zero
-        minconv = helpers.test_gradient_array(ieqcons_J, 
-                     ieqcons_dJ, 
-                     x=numpy.array([0, 0, 10, 4, 20, 8]), 
-                     seed=0.1)
+        x = numpy.array([0, 0, 10, 4, 20, 8])
+        minconv = helpers.test_gradient_array(ieqcons_J, ieqcons_dJ, x=x,
+                                              seed=0.1)
 
         # These constraints are linear so we expect no convergence at all.
         # Let's check that the tolerance is not above a threshold
