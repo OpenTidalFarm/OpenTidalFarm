1273d28b1561908c58f0a44cce1d45cb3384f366
George Barnett <george.barnett10@imperial.ac.uk>
2014-10-12 10:56:28 +0000

    Update test_gradient. FAILS friction. PASSES position.

--- tests/gradient_verification_discrete/test_gradient.py
+++ tests/gradient_verification_discrete/test_gradient.py
@@ -4,23 +4,27 @@ from opentidalfarm import *
 def model(controls, problem_params, sin_ic):
     domain = domains.RectangularDomain(0, 0, 3000, 1000, 5, 5)
 
-    # Create tidal farm
-    farm = TidalFarm(domain)
-    farm.params["controls"] = controls
-    farm.params["output_turbine_power"] = False
-    # The turbine position is the control variable 
-    farm.params["turbine_pos"] = [[1000, 400], [2000, 600]] 
-    # Choosing a friction coefficient of > 0.02 ensures that 
+    # Choosing a friction coefficient of > 0.02 ensures that
     # overlapping turbines lead to less power output.
-    nb_turbines = len(farm.params["turbine_pos"])
-    farm.params["turbine_friction"] = 0.2*numpy.ones(nb_turbines)
-    farm.params["turbine_x"] = 400
-    farm.params["turbine_y"] = 400
+    turbine = BumpTurbine(diameter=400., friction=0.2, controls=controls)
+
+    # Create tidal farm
+    farm = RectangularFarm(domain,
+                           site_x_start=0,
+                           site_x_end=3000,
+                           site_y_start=0,
+                           site_y_end=1000,
+                           turbine=turbine)
+
+    options["output_turbine_power"] = False
+    # The turbine position is the control variable
+    farm.add_turbine((1000,400))
+    farm.add_turbine((2000,600))
     problem_params.tidal_farm = farm
 
     # Domain
     problem_params.domain = domain
-  
+
     # Temporal settings
     period = 1.24*60*60 # Wave period
     problem_params.start_time = Constant(period/4)
@@ -28,30 +32,30 @@ def model(controls, problem_params, sin_ic):
     problem_params.finish_time = Constant(problem_params.start_time + \
             2*problem_params.dt)
     problem_params.theta = Constant(0.5)
-    problem_params.include_advection = True 
-    problem_params.include_viscosity = True 
+    problem_params.include_advection = True
+    problem_params.include_viscosity = True
     problem_params.viscosity = 20.0
-  
+
     # Boundary condition settings
     k = Constant(2*pi/(period*sqrt(problem_params.g*problem_params.depth)))
     eta0 = 2
-    expression = Expression(("eta0*sqrt(g/depth)*cos(k*x[0]-sqrt(g*depth)*k*t)", 
-        "0"), 
-        eta0=eta0, 
-        g=problem_params.g, 
+    expression = Expression(("eta0*sqrt(g/depth)*cos(k*x[0]-sqrt(g*depth)*k*t)",
+        "0"),
+        eta0=eta0,
+        g=problem_params.g,
         depth=problem_params.depth,
-        t=problem_params.start_time, 
+        t=problem_params.start_time,
         k=k)
     bcs = BoundaryConditionSet()
     bcs.add_bc("u", expression, 1, "strong_dirichlet")
     bcs.add_bc("u", expression, 2, "strong_dirichlet")
     bcs.add_bc("u", expression, 3, "strong_dirichlet")
     problem_params.bcs = bcs
-  
+
     # Initial condition
-    problem_params.initial_condition = sin_ic(eta0, 
+    problem_params.initial_condition = sin_ic(eta0,
             k, problem_params.depth, problem_params.start_time)
-  
+
     # Physical parameters
     problem_params.friction = 0.0025
 
@@ -73,22 +77,22 @@ class TestDiscreteTurbine(object):
 
     def test_gradient_of_peak_friction_passes_taylor_test(self,
             sw_linear_problem_parameters, sin_ic):
-        rf = model(["turbine_pos"], sw_linear_problem_parameters, sin_ic)
-        m0 = rf.solver.problem.parameters.tidal_farm.control_array()
+        rf = model(Controls(position=True), sw_linear_problem_parameters, sin_ic)
+        m0 = rf.solver.problem.parameters.tidal_farm.control_array
 
         p = numpy.random.rand(len(m0))
-        minconv = helpers.test_gradient_array(rf.evaluate, rf.derivative, m0, 
+        minconv = helpers.test_gradient_array(rf.evaluate, rf.derivative, m0,
                 seed=0.1, perturbation_direction=p, number_of_tests=4)
 
         assert minconv > 1.97
 
     def test_gradient_of_position_passes_taylor_test(self,
             sw_linear_problem_parameters, sin_ic):
-        rf = model(["turbine_friction"], sw_linear_problem_parameters, sin_ic)
-        m0 = rf.solver.problem.parameters.tidal_farm.control_array()
+        rf = model(Controls(friction=True), sw_linear_problem_parameters, sin_ic)
+        m0 = rf.solver.problem.parameters.tidal_farm.control_array
 
         p = numpy.random.rand(len(m0))
-        minconv = helpers.test_gradient_array(rf.evaluate, rf.derivative, m0, 
+        minconv = helpers.test_gradient_array(rf.evaluate, rf.derivative, m0,
                 seed=0.1, perturbation_direction=p, number_of_tests=4)
 
         assert minconv > 1.97
