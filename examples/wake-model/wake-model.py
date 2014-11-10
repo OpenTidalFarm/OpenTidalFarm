35d1c5b8f63c2b3b335e4d2221d548937d16795d
George Barnett <george.barnett10@imperial.ac.uk>
2014-11-10 16:13:50 +0000

    Update the wake model example. Remove the mesh file.

--- examples/wake-model/wake-model.py
+++ examples/wake-model/wake-model.py
@@ -2,7 +2,16 @@ from opentidalfarm import *
 import numpy
 
 # Create a rectangular domain.
-domain = FileDomain("mesh/mesh.xml")
+# In this setup the wake model does not actually use the mesh, but the requires
+# a domain which by necessesity has a mesh, thus the low number of mesh elements
+# (10x10) does not matter.
+x0 = 0.
+y0 = 0.
+x1 = 640.
+y1 = 320.
+nx = 10
+ny = 10
+domain = RectangularDomain(x0, y0, x1, y1, nx, ny)
 
 # The next step is to create the turbine farm. In this case, the
 # farm consists of 32 turbines, initially deployed in a regular grid layout.
@@ -26,14 +35,14 @@ def flow(x):
     """A dummy flow function where the flow is (1., 0.) everywhere."""
     return numpy.array([2.0, 0.0])
 
-# Set the shallow water parameters
+# Set the Wake problem.
 prob_params = SteadyWakeProblem.default_parameters()
 prob_params.domain = domain
-# The dummy model simply halves the flow speed if a turbine is in the wake of
-# another.
+# Get the default paramteres from the Jensen model.
 model_params = Jensen.default_parameters()
 model_params.turbine_radius = turbine.radius
 prob_params.wake_model = Jensen(model_params, flow)
+# At present this is the only combination model that is correctly working.
 prob_params.combination_model = GeometricSum
 prob_params.tidal_farm = farm
 
@@ -47,9 +56,9 @@ solver = SteadyWakeSolver(problem)
 # use the WakePowerFunctional class.
 functional = WakePowerFunctional(farm)
 
-# The power is calculated by calling the 'power' method of the function with a
-# list of velocities to calculate the power for. At present this power is by no
-# means accurate (we simply take the cube of the flow velocity at each of the
-# turbines).
+# The power is calculated by calling the "power" method of the functional with a
+# list of velocities to calculate the power for. At present this POWER DOES
+# NOT REPRESENT AN ACCURATE POWER in watts! (We simply take the cube of the flow
+# velocity at each of the turbines.)
 flow_velocities = solver.solve()
 print functional.power(flow_velocities)
