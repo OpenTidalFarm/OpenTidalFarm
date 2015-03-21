428307b84cae656cbbd312e5954345e04951681a
Simon Funke <simon.funke@gmail.com>
2015-03-21 22:08:06 +0100

    Improved text for the headland example

--- examples/headland-simulation/headland-simulation.py
+++ examples/headland-simulation/headland-simulation.py
@@ -1,22 +1,33 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# .. _channel_simulation:
+# .. _headland_simulation:
 #
 # .. py:currentmodule:: opentidalfarm
 #
-# Sinusoidal wave in a channel
-# ============================
+# Sinusoidal wave in a headland channel
+# =====================================
 #
 #
 # Introduction
 # ************
 #
-# This example simulates the flow in a channel with oscillating velocity
-# in-/outflow on the west boundary, fixed surface height on the east boundary,
-# and free-slip flow on the north and south boundaries.
+# This example simulates the flow in a headland channel with oscillating
+# head-driven flow. It shows how to
+#   - read in an external mesh file;
+#   - apply boundary conditions for a sinusoidal, head-driven flow;
+#   - solve the transient shallow water equations;
+#   - compute vorticity of the flow field;
+#   - save output files to disk.
 #
-# The shallow water equations to be solved are
+#
+# After a few timesteps the solution looks like this:
+
+# .. image:: simulation_result.png
+#     :scale: 40
+#     :align: center
+#
+# The equations to be solved are the shallow water equations
 #
 # .. math::
 #       \frac{\partial u}{\partial t} +  u \cdot \nabla  u - \nu \nabla^2 u  + g \nabla \eta + \frac{c_b}{H} \| u \|  u = 0, \\
@@ -32,25 +43,6 @@
 # - :math:`\nu` is the viscosity coefficient,
 # - :math:`g` is the gravitational constant.
 #
-# The boundary conditions are:
-#
-# .. math::
-#       u = \begin{pmatrix}\sin(\frac{\pi t}{5}) \frac{y (50-y)}{625}\\0\end{pmatrix} & \quad \textrm{on } \Gamma_1, \\
-#       \eta = 0 & \quad \textrm{on } \Gamma_2, \\
-#       u \cdot n = 0 & \quad \textrm{on } \Gamma_3, \\
-#
-# where :math:`n` is the normal vector pointing outside the domain,
-# :math:`\Gamma_1` is the west boundary of the channel, :math:`\Gamma_2` is the
-# east boundary of the channel, and :math:`\Gamma_3` is the north and south
-# boundaries of the channel.
-#
-#
-
-# After a few timesteps the solution should looks like this:
-
-# .. image:: simulation_result.png
-#     :scale: 40
-#     :align: center
 
 # Implementation
 # **************
@@ -65,54 +57,37 @@ from opentidalfarm import *
 
 prob_params = SWProblem.default_parameters()
 
-# First we define the computational domain. For a simple channel, we can use the
-# :class:`RectangularDomain` class:
+# First we define the computational domain. We load a previously generated mesh
+# (using ```Gmsh``` and converted with ```dolfin-convert```):
+
 
 domain = FileDomain("mesh/headland.xml")
-
-# The boundary of the domain is marked with integers in order to specify
-# different boundary conditions on different parts of the domain. You can plot
-# and inspect the boundary ids with:
-
-#plot(domain.facet_ids)
-#interactive()
-
-# Once the domain is created we attach it to the problem parameters:
-
 prob_params.domain = domain
 
-# Next we specify boundary conditions. For time-dependent boundary condition use
-# a parameter named `t` in the :class:`dolfin.Expression` and it will be automatically be
-# updated to the current timelevel during the solve.
+# Next we specify the boundary conditions.  The parameter `t` in the
+# :class:`dolfin.Expression` is special in OpenTidalFarm as it will be
+# automatically updated to the current timelevel during the solve.
 
 # We assume a propagating wave in the headland case and compute the phase
 # difference to drive flow in the channel.
-
 tidal_amplitude = 2.
 tidal_period = 12.42*60*60 # M2 tidal period
 H = 80 # channel depth
+eta_channel = "amp*sin(omega*t + omega/pow(g*H, 0.5)*x[0])"
 
 bcs = BoundaryConditionSet()
-eta_channel = "amp*sin(omega*t + omega/pow(g*H, 0.5)*x[0])"
 eta_expr = Expression(eta_channel, t=Constant(0), amp=tidal_amplitude,
                       omega=2*pi/tidal_period, g=9.81, H=H)
-bcs.add_bc("eta", eta_expr, facet_id=1, bctype="strong_dirichlet")
-bcs.add_bc("eta", eta_expr, facet_id=2, bctype="strong_dirichlet")
-
-# No-slip boundary conditions on the headland sides are enforced by using
-# `strong_dirichlet` boundary conditions.
-
-bcs.add_bc("u", Constant((0, 0)), facet_id=3, bctype="strong_dirichlet")
-
-# Again we attach boundary conditions to the problem parameters:
+bcs.add_bc("eta", eta_expr, facet_id=1, bctype="strong_dirichlet") # west
+bcs.add_bc("eta", eta_expr, facet_id=2, bctype="strong_dirichlet") # east
+bcs.add_bc("u", Constant((0, 0)), facet_id=3, bctype="strong_dirichlet")  # coast
 
 prob_params.bcs = bcs
 
 # The other parameters are straight forward:
 
 # Equation settings
-nu = 20
-prob_params.viscosity = Constant(nu)
+prob_params.viscosity = Constant(20)
 prob_params.depth = Constant(H)
 prob_params.friction = Constant(0.0025)
 # Temporal settings
@@ -123,7 +98,7 @@ prob_params.dt = Constant(tidal_period/100)
 # The initial condition consists of three components: u_x, u_y and eta
 # Note that we do not set all components to zero, as some components of the
 # Jacobian of the quadratic friction term is non-differentiable.
-prob_params.initial_condition = Expression(("1e-7", "0", eta_channel), t=Constant(0),
+prob_params.initial_condition = Expression(("DOLFIN_EPS", "0", eta_channel), t=Constant(0),
               amp=tidal_amplitude, omega=2*pi/tidal_period, g=9.81, H=H)
 
 # Here we only set the necessary options. A full option list with its current
@@ -143,9 +118,9 @@ sol_params = CoupledSWSolver.default_parameters()
 sol_params.dump_period = -1
 solver = CoupledSWSolver(problem, sol_params)
 
-# Now we are ready to solve the problem.
+# In this example we would also like to compute the vorticity of the flow field.
+# The following FEniCS code solves for the vorticity by a L2 projection
 
-# Compute vorticity by L2 projection
 class VorticitySolver(object):
     def __init__(self, V):
         self.u = Function(V)
@@ -165,46 +140,46 @@ class VorticitySolver(object):
         solve(self.a_mat, self.vort.vector(), L_mat, annotate=False)
         return self.vort
 
-# Create output files
-output_dir = "outputs"
-u_xdmf = XDMFFile(mpi_comm_world(), "{}/u.xdmf".format(output_dir))
-eta_xdmf = XDMFFile(mpi_comm_world(), "{}/eta.xdmf".format(output_dir))
-vort_xdmf = XDMFFile(mpi_comm_world(), "{}/vorticity.xdmf".format(output_dir))
+# We also create some output files to store the results
 
-u_xdmf.parameters["rewrite_function_mesh"] = False
+u_xdmf = XDMFFile(mpi_comm_world(), "outputs/u.xdmf")
+eta_xdmf = XDMFFile(mpi_comm_world(), "outputs/eta.xdmf")
+vort_xdmf = XDMFFile(mpi_comm_world(), "outputs/vorticity.xdmf")
 u_xdmf.parameters["flush_output"] = True
-eta_xdmf.parameters["rewrite_function_mesh"] = False
 eta_xdmf.parameters["flush_output"] = True
-vort_xdmf.parameters["rewrite_function_mesh"] = False
 vort_xdmf.parameters["flush_output"] = True
 
-initialised = False
+# Create the vorticity solver and temporary functions for writing out velocity
+# and surface elevation
+
+V = solver.function_space.extract_sub_space([0]).collapse()
+Q = solver.function_space.extract_sub_space([1]).collapse()
+vort_solver = VorticitySolver(V)
+u = Function(V)
+eta = Function(Q)
+
+# Now we cal run the time loop
+
 for s in solver.solve(annotate=False):
     print "Computed solution at time %f" % s["time"]
-    if not initialised:
-        initialised = True
-        V = s["state"].function_space().extract_sub_space([0]).collapse()
-        Q = s["state"].function_space().extract_sub_space([1]).collapse()
-        vort_solver = VorticitySolver(V)
 
-        u = Function(V)
-        eta = Function(Q)
-
+    # Write velocity, surface elevation and vorticity to files
     u.assign(project(s["u"]), V)
-    eta.assign(project(s["eta"]), Q)
-    vort = vort_solver.solve(u)
-
     u_xdmf << u, float(s["time"])
+
+    eta.assign(project(s["eta"]), Q)
     eta_xdmf << eta, float(s["time"])
+
+    vort = vort_solver.solve(u)
     vort_xdmf << vort, float(s["time"])
 
 # The inner part of the loop is executed for each timestep. The variable :attr:`s`
 # is a dictionary and contains information like the current timelevel, the velocity and
 # free-surface functions.
 
-# The example code can be found in ``examples/channel-simulation/`` in the
-# ``OpenTidalFarm`` source tree, and executed as follows:
+# The example code can be found in ``examples/headland-simulation/`` in the
+# ``OpenTidalFarm`` source tree, and run with:
 
 # .. code-block:: bash
 
-#   $ python channel-simulation.py
+#   $ python headland-simulation.py
