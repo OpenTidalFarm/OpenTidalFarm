ac39c4531f0101ae574f426464c39b626732118e
Simon Funke <simon.funke@gmail.com>
2016-06-24 15:46:23 +0200

    Update conftest.py after doflin-adjoint updates

--- tests/conftest.py
+++ tests/conftest.py
@@ -10,12 +10,6 @@ from fixtures import steady_sw_problem_parameters
 from fixtures import multi_steady_sw_problem_parameters
 from fixtures import sin_ic
 
-del dolfin_adjoint.test_initial_condition_adjoint
-del dolfin_adjoint.test_initial_condition_tlm
-del dolfin_adjoint.test_scalar_parameters_adjoint
-del dolfin_adjoint.test_initial_condition_adjoint_cdiff
-del dolfin_adjoint.test_scalar_parameter_adjoint
-
 default_params = dolfin.parameters.copy()
 def pytest_runtest_setup(item):
     """ Hook function which is called before every test """
