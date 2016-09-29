75516f8755bbb6b9def33bbbe96cfe4bd9fd75c7
Simon Funke <simon.funke@gmail.com>
2016-09-29 09:42:44 +0200

    Updtes for channel simulaion

--- examples/channel-simulation/channel-simulation.py
+++ examples/channel-simulation/channel-simulation.py
@@ -65,6 +65,7 @@
 # We begin with importing the OpenTidalFarm module.
 
 from opentidalfarm import *
+set_log_level(ERROR)  # Tell solver to only print error messages
 
 # Next we get the default parameters of a shallow water problem and configure it
 # to our needs.
@@ -77,10 +78,11 @@ prob_params = SWProblem.default_parameters()
 domain = RectangularDomain(x0=0, y0=0, x1=100, y1=50, nx=20, ny=10)
 
 # The boundary of the domain is marked with integers in order to specify
-# different boundary conditions on different parts of the domain. You can plot
-# and inspect the boundary ids with:
+# different boundary conditions on different parts of the domain. We create a
+# plot of these facets ids to inspect the boundary ids with (open the file `facet_ids.xdmf` in
+# [Paraview](http://www.paraview.org/) to view the plot:
 
-plot(domain.facet_ids)
+XDMFFile("facet_ids.xdmf").write(domain.facet_ids)
 
 # Once the domain is created we attach it to the problem parameters:
 
@@ -138,24 +140,21 @@ sol_params.dump_period = -1
 solver = CoupledSWSolver(problem, sol_params)
 
 # We create some files to store the velocity, u, and surface elevation, eta, at
-# each timestep, so it can be viewed in ParaView later. 
+# each timestep, so it can be viewed in ParaView later.
 
-f_u = XDMFFile(mpi_comm_world(), "u.xdmf")
-f_eta = XDMFFile(mpi_comm_world(), "eta.xdmf")
+f_u = XDMFFile("u.xdmf")
+f_eta = XDMFFile("eta.xdmf")
 
 # Now we are ready to solve the problem.
 
 for s in solver.solve():
     print "Computed solution at time %f" % s["time"]
+
+    # Split the velocity and pressure into its components and write them to
+    # file.
     u, eta = s["state"].split()
     f_u.write(u)
     f_eta.write(eta)
-    plot(u, title="u at time: {}".format(s["time"]))
-    plot(eta, title="eta at time: {}".format(s["time"]))
-
-# (Plots is not shown in docker. View the stored xdmf-files instead.)  
-interactive()  # Hold the plot until the user presses q. 
-
 
 # The inner part of the loop is executed for each timestep. The variable :attr:`s`
 # is a dictionary and contains information like the current timelevel, the velocity and
@@ -170,3 +169,7 @@ interactive()  # Hold the plot until the user presses q.
 # .. code-block:: bash
 
 #   $ python channel-simulation.py
+
+# The results are stored in the files `u.xdmf` and `eta.xdmf`, and can be viewed
+# with [Paraview](http://www.paraview.org/).
+
